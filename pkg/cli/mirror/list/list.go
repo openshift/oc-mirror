cc22603ae1ef893b0650b696c19b549da8c510d2
Jennifer Power <barnabei.jennifer@gmail.com>
2021-11-12 12:53:31 -0500

    docs: README and example update

--- pkg/cli/mirror/list/list.go
+++ pkg/cli/mirror/list/list.go
@@ -13,16 +13,16 @@ func NewListCommand(f kcmdutil.Factory, ro *cli.RootOptions) *cobra.Command {
 		Use:   "list",
 		Short: "List available platform and operator content and their version",
 		Example: templates.Examples(`
-			# Output default operator channles
-			oc-mirror list operators --channels
+			# Output operator catalogs
+			oc-mirror list operators --version=4.9
 			# Output OCP release channel list
-            oc-mirror list releases --channels
+            oc-mirror list releases --channels --version=4.9
 			# List all OCP versions in a specified channel
-			oc-mirror list releases --channel=stable-4.8 --versions
-			# List all operators in a channel
-			oc-mirror list operators --channel=channel-name --packages
+			oc-mirror list releases --channel=stable-4.8
+			# List all operators in a catalog
+			oc-mirror list operators --catalog=catalog-name
 			# List all available versions for a specified operator
-			oc-mirror list operators --channel=channel-name --package=operator-name --versions
+			oc-mirror list operators --catalog=catalog-name --channel=channel-name --package=operator-name
 			# List updates between remote and current workspace
 			oc-mirror list updates --config mirror-config.yaml
 		`),
