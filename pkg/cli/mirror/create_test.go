5ba2ff2ed060ac2faffb17201109cabb19ca28b8
Eric Stroczynski <ericstroczynski@gmail.com>
2021-11-12 13:57:40 -0800

    fix(operator): only add OPM image when catalogs are configured and not previously added
    Signed-off-by: Eric Stroczynski <ericstroczynski@gmail.com>

---
+++ pkg/cli/mirror/create_test.go
@@ -0,0 +1,50 @@
+package mirror
+
+import (
+	"testing"
+
+	"github.com/RedHatGov/bundle/pkg/config/v1alpha1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddOPMImage(t *testing.T) {
+
+	var cfg v1alpha1.ImageSetConfiguration
+	var meta v1alpha1.Metadata
+
+	// No past OPMImage.
+	cfg = v1alpha1.ImageSetConfiguration{}
+	meta = v1alpha1.Metadata{}
+	meta.MetadataSpec.PastMirrors = []v1alpha1.PastMirror{
+		{
+			Mirror: v1alpha1.Mirror{
+				AdditionalImages: []v1alpha1.AdditionalImages{
+					{Image: v1alpha1.Image{Name: "reg.com/ns/other:latest"}},
+				},
+			},
+		},
+	}
+
+	addOPMImage(&cfg, meta)
+	if assert.Len(t, cfg.Mirror.AdditionalImages, 1) {
+		require.Equal(t, cfg.Mirror.AdditionalImages[0].Image.Name, OPMImage)
+	}
+
+	// Has past OPMImage.
+	cfg = v1alpha1.ImageSetConfiguration{}
+	meta = v1alpha1.Metadata{}
+	meta.MetadataSpec.PastMirrors = []v1alpha1.PastMirror{
+		{
+			Mirror: v1alpha1.Mirror{
+				AdditionalImages: []v1alpha1.AdditionalImages{
+					{Image: v1alpha1.Image{Name: OPMImage}},
+					{Image: v1alpha1.Image{Name: "reg.com/ns/other:latest"}},
+				},
+			},
+		},
+	}
+
+	addOPMImage(&cfg, meta)
+	require.Len(t, cfg.Mirror.AdditionalImages, 0)
+}
