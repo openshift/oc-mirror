e7593082232a0371abe08461fef7c7137ccb16a5
Jennifer Power <barnabei.jennifer@gmail.com>
2022-01-12 10:36:21 -0500

    fix(publish): add name options to fix catalog rebuild with HTTP regis… (#263)
    * fix(publish): add name options to fix catalog rebuild with HTTP registries
    
    Fixes #262
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>
    
    * chore: change localhost to localhost.localdomain to better respresent user scenarios

--- test/testcases.sh
+++ test/testcases.sh
@@ -12,91 +12,91 @@ TESTCASES[9]="helm_local"
 # Test full catalog mode.
 function full_catalog () {
     run_full imageset-config-full.yaml true
-    check_bundles localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
+    check_bundles localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.0.0 baz.v1.0.1 baz.v1.1.0 foo.v0.1.0 foo.v0.2.0 foo.v0.3.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 }
 
 # Test heads-only mode
 function headsonly_diff () {
     run_full imageset-config-headsonly.yaml true
-    check_bundles localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
+    check_bundles localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.1.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 
     run_diff imageset-config-headsonly.yaml
-    check_bundles localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
+    check_bundles localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.1.0 foo.v0.3.1 foo.v0.3.2" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 }
 
 # Test registry backend
 function registry_backend () {
     run_full imageset-config-headsonly-backend-registry.yaml true
-    check_bundles localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
+    check_bundles localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.1.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 
     run_diff imageset-config-headsonly-backend-registry.yaml
-    check_bundles localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
+    check_bundles localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.1.0 foo.v0.3.1 foo.v0.3.2" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 }
 
 # Test mirror to mirror with local backend
 function mirror_to_mirror() {
     mirror2mirror imageset-config-headsonly.yaml
-    check_bundles localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
+    check_bundles localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.1.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 }
 
 # Test mirror to mirror no backend
 function mirror_to_mirror_nostorage() {
     mirror2mirror imageset-config-full.yaml
-    check_bundles localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
+    check_bundles localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.0.0 baz.v1.0.1 baz.v1.1.0 foo.v0.1.0 foo.v0.2.0 foo.v0.3.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 }
 
 # Test registry backend with custom namespace
 function custom_namespace {
     run_full imageset-config-headsonly-backend-registry.yaml true "custom"
-    check_bundles "localhost:${REGISTRY_DISCONN_PORT}/custom/${CATALOGNAMESPACE}:test-catalog-latest" \
+    check_bundles "localhost.localdomain:${REGISTRY_DISCONN_PORT}/custom/${CATALOGNAMESPACE}:test-catalog-latest" \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.1.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT} "custom"
+    localhost.localdomain:${REGISTRY_DISCONN_PORT} "custom"
 
     run_diff imageset-config-headsonly-backend-registry.yaml "custom"
-    check_bundles "localhost:${REGISTRY_DISCONN_PORT}/custom/${CATALOGNAMESPACE}:test-catalog-latest" \
+    check_bundles "localhost.localdomain:${REGISTRY_DISCONN_PORT}/custom/${CATALOGNAMESPACE}:test-catalog-latest" \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.1.0 foo.v0.3.1 foo.v0.3.2" \
-    localhost:${REGISTRY_DISCONN_PORT} "custom"
+    localhost.localdomain:${REGISTRY_DISCONN_PORT} "custom"
 }
 
 
 # Test package filtering
 function package_filtering {
     run_full imageset-config-filter.yaml true
-    check_bundles "localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest" \
+    check_bundles "localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest" \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 foo.v0.1.0 foo.v0.2.0 foo.v0.3.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 
     run_diff imageset-config-filter-multi.yaml
-    check_bundles "localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest" \
+    check_bundles "localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest" \
     "bar.v0.1.0 bar.v0.2.0 bar.v1.0.0 baz.v1.0.1 baz.v1.1.0 foo.v0.1.0 foo.v0.2.0 foo.v0.3.0 foo.v0.3.1 foo.v0.3.2" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 }
 
 # Test skip deps
 function skip_deps {
     run_full imageset-config-skip-deps.yaml true
-    check_bundles "localhost:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest" \
+    check_bundles "localhost.localdomain:${REGISTRY_DISCONN_PORT}/${CATALOGNAMESPACE}:test-catalog-latest" \
     "bar.v1.0.0 baz.v1.1.0 foo.v0.3.1" \
-    localhost:${REGISTRY_DISCONN_PORT}
+    localhost.localdomain:${REGISTRY_DISCONN_PORT}
 }
 
 
 # Test local helm chart
 function helm_local {
     run_helm imageset-config-helm.yaml podinfo-6.0.0.tgz
-    check_helm "localhost:${REGISTRY_DISCONN_PORT}/stefanprodan/podinfo:6.0.0"
+    check_helm "localhost.localdomain:${REGISTRY_DISCONN_PORT}/stefanprodan/podinfo:6.0.0"
 }
