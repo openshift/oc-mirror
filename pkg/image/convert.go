27dba0bf346dc392cf62fac706378cd2241ba241
Jennifer Power <barnabei.jennifer@gmail.com>
2022-04-06 18:05:59 -0400

    fix: changes ConvertToAssociationSet methods to allow for shared child manifest
    Currently, if two images with an index manifest contain the same child manifest,
    the conversion to an AssociationSet set fails because child manifests records are being
    deleted after processing to identify parent image manifests. This change adds the child manifest
    to a visited map and checks if it exists instead to allow for shared child manifests.
    
    Fixes #402
    
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- pkg/image/convert.go
+++ pkg/image/convert.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 
 	"github.com/openshift/oc-mirror/pkg/api/v1alpha2"
+	"github.com/sirupsen/logrus"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 )
 
 // ConvertToAssociationSet will return an AssociationSet from a slice of Associations
-func ConvertToAssociationSet(assoc []v1alpha2.Association) (AssociationSet, error) {
+func ConvertToAssociationSet(assocs []v1alpha2.Association) (AssociationSet, error) {
 	assocSet := AssociationSet{}
 
-	assocMapping := make(map[string]v1alpha2.Association, len(assoc))
+	assocMapping := make(map[string]v1alpha2.Association, len(assocs))
 	var errs []error
-	for _, a := range assoc {
+	for _, a := range assocs {
 		if err := a.Validate(); err != nil {
 			errs = append(errs, err)
+			continue
 		}
 		assocMapping[a.Name] = a
 	}
@@ -24,27 +26,31 @@ func ConvertToAssociationSet(assoc []v1alpha2.Association) (AssociationSet, erro
 	}
 
 	// Process and remove all index manifest and the child image manifests
-	for key, value := range assocMapping {
+	visited := make(map[string]struct{}, len(assocMapping))
+	for _, value := range assocMapping {
 		if len(value.ManifestDigests) != 0 {
 			if ok := assocSet.SetContainsKey(value.Name); !ok {
 				assocSet[value.Name] = make(Associations)
 			}
 			assocSet.Add(value.Name, value)
 			for _, digest := range value.ManifestDigests {
+				logrus.Debugf("image %q: processing child manifest %s", value.Name, digest)
 				child, ok := assocMapping[digest]
 				if !ok {
 					return assocSet, fmt.Errorf("invalid associations: association for %q is missing", digest)
 				}
 				assocSet.Add(value.Name, child)
-				delete(assocMapping, digest)
+				visited[child.Name] = struct{}{}
 			}
-			delete(assocMapping, key)
+			visited[value.Name] = struct{}{}
 		}
 	}
 
 	// Process image manifests with no parent
 	for _, value := range assocMapping {
-		assocSet.Add(value.Name, value)
+		if _, found := visited[value.Name]; !found {
+			assocSet.Add(value.Name, value)
+		}
 	}
 
 	return assocSet, nil
@@ -58,6 +64,7 @@ func ConvertFromAssociationSet(assocSet AssociationSet) ([]v1alpha2.Association,
 		for _, a := range as {
 			if err := a.Validate(); err != nil {
 				errs = append(errs, err)
+				continue
 			}
 			assocs = append(assocs, a)
 		}
