0eff80a042980f7fe24def9df386edfa6667969a
Jennifer Power <barnabei.jennifer@gmail.com>
2022-04-20 22:09:11 -0400

    fix: adds a length check to the digest to avoid out of range on getPartialDigest
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- pkg/image/digest.go
+++ pkg/image/digest.go
@@ -1,13 +1,25 @@
 package image
 
-import "github.com/opencontainers/go-digest"
+import (
+	"fmt"
 
-// getPartialDigest returns the first 6 characters of a digest
-// if valid.
+	"github.com/opencontainers/go-digest"
+)
+
+// getPartialDigest returns the first 6 characters of a digest,
+// if valid. The purpose is to use this as a unique image tag.
+// The since the algorithm and deletimeter are the first 7 characters,
+// range 7-13 is used here.
+// Ex. sha256:fc07c1e2a5f012320ae672ca8546ff0d09eb8dba3c5acbbfc426c7984169ee84
+// would result in fc07c1.
 func getPartialDigest(d string) (string, error) {
 	_, err := digest.Parse(d)
 	if err != nil {
 		return "", err
+	}
+
+	if len(d) < 13 {
+		return "", fmt.Errorf("digest %q does not meet length requirements for partial calculations", d)
 	}
 	return d[7:13], nil
 }
