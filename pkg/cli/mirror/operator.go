70b50f83c0cdd4ae6acc73c5ed5d62e4f3e5a1b9
Sherine Khoury <skhoury@redhat.com>
2023-02-10 15:00:04 +0100

    Remove unnecessary and panicky update of Operators list in ImageSetConfig

--- pkg/cli/mirror/operator.go
+++ pkg/cli/mirror/operator.go
@@ -101,15 +101,13 @@ func (o *OperatorOptions) run(ctx context.Context, cfg v1alpha2.ImageSetConfigur
 	defer reg.Destroy()
 
 	mmapping := image.TypedImageMapping{}
-	for i, ctlg := range cfg.Mirror.Operators {
+	for _, ctlg := range cfg.Mirror.Operators {
 		// Exclude catalog images with "oci://" from renderDC and plan
 		// These catalogs will be handled with bulkImageMirror instead
 		// TODO: we should be able to use renderDC still for these images
 		// so that the catalog image (filtered) gets recreated (and written
 		// to disk) for the OCI catalogs as well
-		if image.IsFBCOCI(ctlg.Catalog) {
-			cfg.Mirror.Operators = append(cfg.Mirror.Operators[:i], cfg.Mirror.Operators[i+1:]...)
-		} else {
+		if !image.IsFBCOCI(ctlg.Catalog) {
 			ctlgRef, err := image.ParseReference(ctlg.Catalog)
 			if err != nil {
 				return nil, fmt.Errorf("error parsing catalog: %v", err)
