33cd909eee26b50abbf8c8d87bb345743e773f14
John Hunkins <jhunkins@us.ibm.com>
2023-05-09 11:44:01 -0500

    refactor processImageIndex to be a method
    - based on review, make inner function processImageIndex into a method
    of ImageBuilder
    - add/update documentation

--- pkg/image/builder/image_builder.go
+++ pkg/image/builder/image_builder.go
@@ -55,10 +55,32 @@ func (b *ImageBuilder) init() {
 	}
 }
 
+/*
+configUpdateFunc allows callers of ImageBuilder.Run to modify the *v1.ConfigFile argument as appropriate for
+the circumstances.
+*/
 type configUpdateFunc func(*v1.ConfigFile)
 
-// Run modifies and pushes the catalog image existing in an OCI layout. The image configuration will be updated
-// with the required labels and any provided layers will be appended.
+/*
+Run modifies and pushes the catalog image existing in an OCI layout. The image configuration will be updated
+with the required labels and any provided layers will be appended.
+
+# Arguments
+
+• ctx: a cancellation context
+
+• targetRef: a docker image reference
+
+• layoutPath: an OCI image layout path
+
+• update: an optional function that allows callers to modify the *v1.ConfigFile if necessary
+
+• layers: zero or more layers to add to the images discovered during processing
+
+# Returns
+
+error: non-nil on error, nil otherwise
+*/
 func (b *ImageBuilder) Run(ctx context.Context, targetRef string, layoutPath layout.Path, update configUpdateFunc, layers ...v1.Layer) error {
 	b.init()
 	var v2format bool
@@ -88,118 +110,8 @@ func (b *ImageBuilder) Run(ctx context.Context, targetRef string, layoutPath lay
 	}
 	originalIdxManifest = originalIdxManifest.DeepCopy()
 
-	/*
-		processImageIndex is a recursive function that allows for traversal of the hierarchy of
-		parent/child indexes that can exist for a multi arch image. There's always
-		at least one index at the root since this is an OCI layout that we're dealing with.
-		In theory there can be "infinite levels" of "index indirection" for multi arch images, but typically
-		its only two levels deep (i.e. index.json itself which is level one, and the manifest list
-		defined in the blobs directory, which is level two).
-	*/
-	var processImageIndex func(idx v1.ImageIndex) (v1.ImageIndex, error)
-	processImageIndex = func(idx v1.ImageIndex) (v1.ImageIndex, error) {
-		var resultIdx v1.ImageIndex
-		resultIdx = idx
-		idxManifest, err := idx.IndexManifest()
-		if err != nil {
-			return nil, err
-		}
-		for _, manifest := range idxManifest.Manifests {
-			currentHash := *manifest.Digest.DeepCopy()
-			switch manifest.MediaType {
-			case types.DockerManifestList, types.OCIImageIndex:
-				innerIdx, err := idx.ImageIndex(currentHash)
-				if err != nil {
-					return nil, err
-				}
-				// recursive call
-				processedIdx, err := processImageIndex(innerIdx)
-				if err != nil {
-					return nil, err
-				}
-				resultIdx = processedIdx
-				// making an assumption here that at any given point in the parent/child
-				// hierarchy, there's only a single image index entry
-				return resultIdx, nil
-			case types.DockerManifestSchema2:
-				v2format = true
-			case types.OCIManifestSchema1:
-				v2format = false
-			default:
-				return nil, fmt.Errorf("image %q: unsupported manifest format %q", targetRef, manifest.MediaType)
-			}
-
-			img, err := idx.Image(currentHash)
-			if err != nil {
-				return nil, err
-			}
-
-			// Add new layers to image.
-			// Ensure they have the right media type.
-			var mt types.MediaType
-			if v2format {
-				mt = types.DockerLayer
-			} else {
-				mt = types.OCILayer
-			}
-			additions := make([]mutate.Addendum, 0, len(layers))
-			for _, layer := range layers {
-				additions = append(additions, mutate.Addendum{Layer: layer, MediaType: mt})
-			}
-			img, err = mutate.Append(img, additions...)
-			if err != nil {
-				return nil, err
-			}
-
-			if update != nil {
-				// Update image config
-				cfg, err := img.ConfigFile()
-				if err != nil {
-					return nil, err
-				}
-				update(cfg)
-				img, err = mutate.Config(img, cfg.Config)
-				if err != nil {
-					return nil, err
-				}
-			}
-
-			desc, err := partial.Descriptor(img)
-			if err != nil {
-				return nil, err
-			}
-
-			// if the platform is not set, we need to attempt to do something about that
-			if desc.Platform == nil {
-				if manifest.Platform != nil {
-					// use the value from the manifest
-					desc.Platform = manifest.Platform
-				} else {
-					if config, err := img.ConfigFile(); err != nil {
-						// we can't get the config file so fall back to linux/amd64
-						desc.Platform = &v1.Platform{Architecture: "amd64", OS: "linux"}
-					} else {
-						// if one of the required values is missing, fall back to linux/amd64
-						if config.Architecture == "" || config.OS == "" {
-							desc.Platform = &v1.Platform{Architecture: "amd64", OS: "linux"}
-						} else {
-							// use the value provided by the image config
-							desc.Platform = &v1.Platform{Architecture: config.Architecture, OS: config.OS}
-						}
-					}
-				}
-			}
-			add := mutate.IndexAddendum{
-				Add:        img,
-				Descriptor: *desc,
-			}
-			modifiedIndex := mutate.AppendManifests(mutate.RemoveManifests(resultIdx, match.Digests(currentHash)), add)
-			resultIdx = modifiedIndex
-		}
-		return resultIdx, nil
-	}
-
-	resultIdx, err := processImageIndex(idx)
+	// process the image index for updates to images discovered along the way
+	resultIdx, err := b.processImageIndex(ctx, idx, &v2format, update, targetRef, layers...)
 	if err != nil {
 		return err
 	}
@@ -251,6 +163,139 @@ func (b *ImageBuilder) Run(ctx context.Context, targetRef string, layoutPath lay
 	}
 	// push to the remote
 	return remote.WriteIndex(tag, imageIndexToPush, b.RemoteOpts...)
+}
+
+/*
+processImageIndex is a recursive helper function that allows for traversal of the hierarchy of
+parent/child indexes that can exist for a multi arch image. There's always
+at least one index at the root since this is an OCI layout that we're dealing with.
+In theory there can be "infinite levels" of "index indirection" for multi arch images, but typically
+its only two levels deep (i.e. index.json itself which is level one, and the manifest list
+defined in the blobs directory, which is level two).
+
+Each image that is encountered is updated using the update function (if provided) and whatever layers are provided.
+
+# Arguments
+
+• ctx: a cancellation context
+
+• idx: the "current" image index for this stage of recursion
+
+• v2format: a boolean used to keep track of the type of image we're dealing with. false means OCI media types
+should be used and true means docker v2s2 media types should be used
+
+• update: an optional function that allows callers to modify the *v1.ConfigFile if necessary
+
+• targetRef: the docker image reference, which is only used for error reporting in this function
+
+• layers: zero or more layers to add to the images discovered during processing
+
+# Returns
+
+• v1.ImageIndex: The resulting image index after processing has completed. Will be nil if an error occurs, otherwise non-nil.
+
+• error: non-nil if an error occurs, nil otherwise
+*/
+func (b *ImageBuilder) processImageIndex(ctx context.Context, idx v1.ImageIndex, v2format *bool, update configUpdateFunc, targetRef string, layers ...v1.Layer) (v1.ImageIndex, error) {
+	var resultIdx v1.ImageIndex
+	resultIdx = idx
+	idxManifest, err := idx.IndexManifest()
+	if err != nil {
+		return nil, err
+	}
+	for _, manifest := range idxManifest.Manifests {
+		currentHash := *manifest.Digest.DeepCopy()
+		switch manifest.MediaType {
+		case types.DockerManifestList, types.OCIImageIndex:
+			innerIdx, err := idx.ImageIndex(currentHash)
+			if err != nil {
+				return nil, err
+			}
+			// recursive call
+			processedIdx, err := b.processImageIndex(ctx, innerIdx, v2format, update, targetRef, layers...)
+			if err != nil {
+				return nil, err
+			}
+			resultIdx = processedIdx
+			// making an assumption here that at any given point in the parent/child
+			// hierarchy, there's only a single image index entry
+			return resultIdx, nil
+		case types.DockerManifestSchema2:
+			*v2format = true
+		case types.OCIManifestSchema1:
+			*v2format = false
+		default:
+			return nil, fmt.Errorf("image %q: unsupported manifest format %q", targetRef, manifest.MediaType)
+		}
+
+		img, err := idx.Image(currentHash)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add new layers to image.
+		// Ensure they have the right media type.
+		var mt types.MediaType
+		if *v2format {
+			mt = types.DockerLayer
+		} else {
+			mt = types.OCILayer
+		}
+		additions := make([]mutate.Addendum, 0, len(layers))
+		for _, layer := range layers {
+			additions = append(additions, mutate.Addendum{Layer: layer, MediaType: mt})
+		}
+		img, err = mutate.Append(img, additions...)
+		if err != nil {
+			return nil, err
+		}
+
+		if update != nil {
+			// Update image config
+			cfg, err := img.ConfigFile()
+			if err != nil {
+				return nil, err
+			}
+			update(cfg)
+			img, err = mutate.Config(img, cfg.Config)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		desc, err := partial.Descriptor(img)
+		if err != nil {
+			return nil, err
+		}
+
+		// if the platform is not set, we need to attempt to do something about that
+		if desc.Platform == nil {
+			if manifest.Platform != nil {
+				// use the value from the manifest
+				desc.Platform = manifest.Platform
+			} else {
+				if config, err := img.ConfigFile(); err != nil {
+					// we can't get the config file so fall back to linux/amd64
+					desc.Platform = &v1.Platform{Architecture: "amd64", OS: "linux"}
+				} else {
+					// if one of the required values is missing, fall back to linux/amd64
+					if config.Architecture == "" || config.OS == "" {
+						desc.Platform = &v1.Platform{Architecture: "amd64", OS: "linux"}
+					} else {
+						// use the value provided by the image config
+						desc.Platform = &v1.Platform{Architecture: config.Architecture, OS: config.OS}
+					}
+				}
+			}
+		}
+		add := mutate.IndexAddendum{
+			Add:        img,
+			Descriptor: *desc,
+		}
+		modifiedIndex := mutate.AppendManifests(mutate.RemoveManifests(resultIdx, match.Digests(currentHash)), add)
+		resultIdx = modifiedIndex
+	}
+	return resultIdx, nil
 }
 
 /*
