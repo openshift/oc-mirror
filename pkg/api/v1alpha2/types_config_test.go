e06d3097f7d73bf8e987cfb161ce872bab57e019
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2023-06-06 07:17:20 +0000

    Fix OCPBUGS-11840: ParseImageReference supports cases where both tag and digest are present in a ref (#637)
    Co-authored-by: Sherine Khoury <skhoury@redhat.com>

--- pkg/api/v1alpha2/types_config_test.go
+++ pkg/api/v1alpha2/types_config_test.go
@@ -6,6 +6,58 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestParseImageReference(t *testing.T) {
+	type spec struct {
+		desc          string
+		input         string
+		expRegistry   string
+		expNamespace  string
+		expRepository string
+		expDigest     string
+		expTag        string
+	}
+
+	specs := []spec{
+		{
+			desc:          "remote image with both tag and digest",
+			input:         "cp.icr.io/cp/cpd/postgresql:13.7@sha256:e05434bfdb4b306fbc2e697112e1343907e368eb5f348c1779562d31b9f32ac5",
+			expRegistry:   "cp.icr.io",
+			expNamespace:  "cp/cpd",
+			expRepository: "postgresql",
+			expDigest:     "e05434bfdb4b306fbc2e697112e1343907e368eb5f348c1779562d31b9f32ac5",
+			expTag:        "13.7",
+		},
+		{
+			desc:          "remote image with tag",
+			input:         "quay.io/redhatgov/oc-mirror-dev:foo-bundle-v0.3.1",
+			expRegistry:   "quay.io",
+			expNamespace:  "redhatgov",
+			expRepository: "oc-mirror-dev",
+			expDigest:     "",
+			expTag:        "foo-bundle-v0.3.1",
+		},
+		{
+			desc:          "remote image with digest",
+			input:         "quay.io/redhatgov/oc-mirror-dev@sha256:7e1e74b87a503e95db5203334917856f61aece90a72e8d53a9fd903344eb78a5",
+			expRegistry:   "quay.io",
+			expNamespace:  "redhatgov",
+			expRepository: "oc-mirror-dev",
+			expDigest:     "7e1e74b87a503e95db5203334917856f61aece90a72e8d53a9fd903344eb78a5",
+			expTag:        "",
+		},
+	}
+
+	for _, s := range specs {
+		t.Run(s.desc, func(t *testing.T) {
+			reg, ns, repo, tag, id := ParseImageReference(s.input)
+			require.Equal(t, s.expRegistry, reg)
+			require.Equal(t, s.expNamespace, ns)
+			require.Equal(t, s.expRepository, repo)
+			require.Equal(t, s.expTag, tag)
+			require.Equal(t, s.expDigest, id)
+		})
+	}
+}
 func TestGetUniqueName(t *testing.T) {
 	type spec struct {
 		desc     string
