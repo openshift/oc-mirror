f8bf34a7750d392ee252697c0fb85e912af74e23
skhoury <skhoury@redhat.com>
2023-10-27 11:26:08 +0200

    Fix E2E tests by only calling InitReexec in main when --v2 is in args

--- cmd/oc-mirror/main.go
+++ cmd/oc-mirror/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"os"
+
 	"github.com/containers/buildah"
 	"github.com/containers/storage/pkg/unshare"
 	"github.com/openshift/oc-mirror/pkg/cli/mirror"
+	"golang.org/x/exp/slices"
 	kcmdutil "k8s.io/kubectl/pkg/cmd/util"
 )
 
@@ -11,11 +14,12 @@ func main() {
 	// oc-mirror runs in rootless mode. It is therefore necessary to
 	// ensure that oc-mirror is re-executed in a user namespace where
 	// it has root privileges.
-	if buildah.InitReexec() {
-		return
+	if len(os.Args) > 0 && slices.Contains(os.Args[:], "--v2") {
+		if buildah.InitReexec() {
+			return
+		}
+		unshare.MaybeReexecUsingUserNamespace(false)
 	}
-	unshare.MaybeReexecUsingUserNamespace(false)
-
 	rootCmd := mirror.NewMirrorCmd()
 	checkErr(rootCmd.Execute())
 }
