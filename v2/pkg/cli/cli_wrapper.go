cc96f6123a029bec200b8e68033f33ddb67fe136
Alex Guidi <122013838+aguidirh@users.noreply.github.com>
2024-11-20 18:08:52 +0100

    clid-279: removes the tech preview warning from v2 (#958)

--- v2/pkg/cli/cli_wrapper.go
+++ v2/pkg/cli/cli_wrapper.go
@@ -16,7 +16,6 @@ func V2Cmd(loglevel string) *cobra.Command {
 	log := clog.New(loglevel)
 
 	fmt.Println()
-	log.Warn("⚠️  --v2 flag identified, flow redirected to the oc-mirror v2 version. This is Tech Preview, it is still under development and it is not production ready.")
 
 	cmd := cli.NewMirrorCmd(log)
 	return cmd
