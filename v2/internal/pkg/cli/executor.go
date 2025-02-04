342136f66647b712082f0808d75903a544c0ef15
sherine-k <skhoury@redhat.com>
2025-02-04 11:17:05 +0100

    CLID-280: WIP: Copy signatures during M2M

--- v2/internal/pkg/cli/executor.go
+++ v2/internal/pkg/cli/executor.go
@@ -433,8 +433,8 @@ func (o *ExecutorSchema) Complete(args []string) error {
 
 	// make sure we always get multi-arch images
 	o.Opts.MultiArch = "all"
-	// for the moment, mirroring doesn't verify signatures. Expected in CLID-26
-	o.Opts.RemoveSignatures = true
+	// forcing signature verification always
+	o.Opts.RemoveSignatures = false
 
 	o.Opts.ParallelLayerImages = maxParallelLayerDownloads
 	if o.ParallelImageLayers > 0 {
