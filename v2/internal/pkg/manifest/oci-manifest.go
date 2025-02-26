c6ddde407d02b583dc3e0392342693493cd8a724
Rafael Fonseca <r4f4rfs@gmail.com>
2025-02-26 19:21:02 +0100

    v2/manifest/oci-manifest: refactor and dedup the code
    I have also added more unit tests.

--- v2/internal/pkg/manifest/oci-manifest.go
+++ v2/internal/pkg/manifest/oci-manifest.go
@@ -10,9 +10,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 
@@ -20,119 +18,99 @@ import (
 	"github.com/containers/image/v5/transports/alltransports"
 	"github.com/containers/image/v5/types"
 	digest "github.com/opencontainers/go-digest"
+	"github.com/otiai10/copy"
 
 	"github.com/openshift/oc-mirror/v2/internal/pkg/api/v2alpha1"
+	"github.com/openshift/oc-mirror/v2/internal/pkg/common"
 	clog "github.com/openshift/oc-mirror/v2/internal/pkg/log"
 	"github.com/openshift/oc-mirror/v2/internal/pkg/mirror"
-	"github.com/otiai10/copy"
 )
-
-var internalLog clog.PluggableLoggerInterface
 
 type Manifest struct {
 	Log clog.PluggableLoggerInterface
 }
 
-func New(log clog.PluggableLoggerInterface) ManifestInterface {
-	internalLog = log
+func New(log clog.PluggableLoggerInterface) *Manifest {
 	return &Manifest{Log: log}
 }
 
 // GetImageIndex - used to get the oci index.json
 func (o Manifest) GetImageIndex(dir string) (*v2alpha1.OCISchema, error) {
-	setInternalLog(o.Log)
-	var oci *v2alpha1.OCISchema
-	indx, err := os.ReadFile(dir + "/" + index)
+	indexPath := filepath.Join(dir, index)
+	oci, err := o.GetImageManifest(indexPath)
 	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(indx, &oci)
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("image index %q: %w", indexPath, err)
 	}
 	return oci, nil
 }
 
-// GetImageManifest used to ge the manifest in the oci blobs/sha254
+// GetImageManifest used to ge the manifest in the oci blobs/sha256
 // directory - found in index.json
 func (o Manifest) GetImageManifest(file string) (*v2alpha1.OCISchema, error) {
-	setInternalLog(o.Log)
-	var oci *v2alpha1.OCISchema
-	manifest, err := os.ReadFile(file)
+	oci, err := common.ParseJsonFile[*v2alpha1.OCISchema](file)
 	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(manifest, &oci)
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("manifest: %w", err)
 	}
 	return oci, nil
 }
 
 // GetOperatorConfig used to parse the operator json
 func (o Manifest) GetOperatorConfig(file string) (*v2alpha1.OperatorConfigSchema, error) {
-	setInternalLog(o.Log)
-	var ocs *v2alpha1.OperatorConfigSchema
-	manifest, err := os.ReadFile(file)
+	ocs, err := common.ParseJsonFile[*v2alpha1.OperatorConfigSchema](file)
 	if err != nil {
-		return nil, err
-	}
-	err = json.Unmarshal(manifest, &ocs)
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("operator config: %w", err)
 	}
 	return ocs, nil
 }
 
 // ExtractLayersOCI
 func (o Manifest) ExtractLayersOCI(fromPath, toPath, label string, oci *v2alpha1.OCISchema) error {
-	setInternalLog(o.Log)
-	if _, err := os.Stat(toPath + "/" + label); errors.Is(err, os.ErrNotExist) {
-		for _, blob := range oci.Layers {
-			validDigest, err := digest.Parse(blob.Digest)
-			if err != nil {
-				return fmt.Errorf("the digest format is not correct %s ", blob.Digest)
-			}
-			f, err := os.Open(fromPath + "/" + validDigest.Encoded())
-			if err != nil {
-				return err
-			}
-			err = untar(f, toPath, label)
-			if err != nil {
-				return err
-			}
-		}
-	} else {
+	_, err := os.Stat(filepath.Join(toPath, label))
+	if err == nil {
 		o.Log.Debug("extract directory exists (nop)")
+		return nil
+	}
+	if !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("extract directory: %w", err)
+	}
+	for _, blob := range oci.Layers {
+		validDigest, err := digest.Parse(blob.Digest)
+		if err != nil {
+			return fmt.Errorf("digest %q: format is not correct: %w", blob.Digest, err)
+		}
+		digestString := validDigest.Encoded()
+		f, err := os.Open(filepath.Join(fromPath, digestString))
+		if err != nil {
+			return fmt.Errorf("digest %q: open origin layer: %w", digestString, err)
+		}
+		if err := untar(f, toPath, label); err != nil {
+			return fmt.Errorf("untar %q: %w", digestString, err)
+		}
 	}
 	return nil
 }
 
 // GetReleaseSchema
 func (o Manifest) GetReleaseSchema(filePath string) ([]v2alpha1.RelatedImage, error) {
-	setInternalLog(o.Log)
-	var release = v2alpha1.ReleaseSchema{}
-
-	file, err := os.ReadFile(filePath)
+	release, err := common.ParseJsonFile[v2alpha1.ReleaseSchema](filePath)
 	if err != nil {
-		return []v2alpha1.RelatedImage{}, err
+		return nil, fmt.Errorf("release schema: %w", err)
 	}
 
-	err = json.Unmarshal([]byte(file), &release)
-	if err != nil {
-		return []v2alpha1.RelatedImage{}, err
-	}
-
-	var allImages []v2alpha1.RelatedImage
+	allImages := make([]v2alpha1.RelatedImage, 0, len(release.Spec.Tags))
 	for _, item := range release.Spec.Tags {
-		allImages = append(allImages, v2alpha1.RelatedImage{Image: item.From.Name, Name: item.Name, Type: v2alpha1.TypeOCPReleaseContent})
+		allImages = append(allImages, v2alpha1.RelatedImage{
+			Image: item.From.Name,
+			Name:  item.Name,
+			Type:  v2alpha1.TypeOCPReleaseContent,
+		})
 	}
 	return allImages, nil
 }
 
 // UntarLayers simple function that untars the image layers
 func untar(gzipStream io.Reader, path string, cfgDirName string) error {
-	//Remove any separators in cfgDirName as received from the label
+	// Remove any separators in cfgDirName as received from the label
 	cfgDirName = strings.TrimSuffix(cfgDirName, "/")
 	cfgDirName = strings.TrimPrefix(cfgDirName, "/")
 	uncompressedStream, err := gzip.NewReader(gzipStream)
@@ -157,20 +135,21 @@ func untar(gzipStream io.Reader, path string, cfgDirName string) error {
 			case tar.TypeDir:
 				if header.Name != "./" {
 					if err := os.MkdirAll(filepath.Join(path, header.Name), 0755); err != nil {
-						return fmt.Errorf("untar: Mkdir() failed: %v", err)
+						return fmt.Errorf("untar: Mkdir() failed: %w", err)
 					}
 				}
 			case tar.TypeReg:
 				err := os.MkdirAll(filepath.Dir(filepath.Join(path, header.Name)), 0755)
 				if err != nil {
-					return fmt.Errorf("untar: Create() failed: %v", err)
+					return fmt.Errorf("untar: Create() failed: %w", err)
 				}
 				outFile, err := os.Create(filepath.Join(path, header.Name))
 				if err != nil {
-					return fmt.Errorf("untar: Create() failed: %v", err)
+					return fmt.Errorf("untar: Create() failed: %w", err)
 				}
 				if _, err := io.Copy(outFile, tarReader); err != nil {
-					return fmt.Errorf("untar: Copy() failed: %v", err)
+					outFile.Close()
+					return fmt.Errorf("untar: Copy() failed: %w", err)
 				}
 				outFile.Close()
 
@@ -185,79 +164,71 @@ func untar(gzipStream io.Reader, path string, cfgDirName string) error {
 // ConvertIndex converts the index.json to a single manifest which refers to a multi manifest index in the blobs/sha256 directory
 // this is necessary because containers/image does not support multi manifest indexes on the top level folder
 func (o Manifest) ConvertIndexToSingleManifest(dir string, oci *v2alpha1.OCISchema) error {
-	setInternalLog(o.Log)
-
-	data, err := os.ReadFile(path.Join(dir, "index.json"))
+	data, err := os.ReadFile(filepath.Join(dir, "index.json"))
 	if err != nil {
-		o.Log.Debug(err.Error())
+		return fmt.Errorf("read index.json: %w", err)
 	}
 	hash := sha256.Sum256(data)
 	digest := hex.EncodeToString(hash[:])
 	size := len(data)
-	log.Println("Digest:", digest)
-	log.Println("Size:", size)
+	o.Log.Debug("Digest:", digest)
+	o.Log.Debug("Size:", size)
 
-	err = copy.Copy(path.Join(dir, "index.json"), path.Join(dir, "blobs", "sha256", digest))
+	err = copy.Copy(filepath.Join(dir, "index.json"), filepath.Join(dir, "blobs", "sha256", digest))
 	if err != nil {
-		return err
+		return fmt.Errorf("copy index.json to destination: %w", err)
 	}
 
 	idx := v2alpha1.OCISchema{
 		SchemaVersion: oci.SchemaVersion,
-		Manifests:     []v2alpha1.OCIManifest{{MediaType: oci.MediaType, Digest: "sha256:" + digest, Size: size}},
+		Manifests: []v2alpha1.OCIManifest{
+			{
+				MediaType: oci.MediaType,
+				Digest:    fmt.Sprintf("sha256:%s", digest),
+				Size:      size,
+			},
+		},
 	}
 
 	idxData, err := json.Marshal(idx)
 	if err != nil {
-		return err
+		return fmt.Errorf("encode manifest OCISchema: %w", err)
 	}
 
 	// Write the JSON string to a file
-	err = os.WriteFile(path.Join(dir, "index.json"), idxData, 0644)
+	err = os.WriteFile(filepath.Join(dir, "index.json"), idxData, 0644) // nolint:gosec // G306: no sensitive data
 	if err != nil {
-		return err
+		return fmt.Errorf("write single manifest index.json: %w", err)
 	}
 
 	return nil
 }
 
 func (o Manifest) GetDigest(ctx context.Context, sourceCtx *types.SystemContext, imgRef string) (string, error) {
-	setInternalLog(o.Log)
-
-	if err := mirror.ReexecIfNecessaryForImages([]string{imgRef}...); err != nil {
-		return "", err
+	if err := mirror.ReexecIfNecessaryForImages(imgRef); err != nil {
+		return "", fmt.Errorf("reexec mirror: %w", err)
 	}
 
 	srcRef, err := alltransports.ParseImageName(imgRef)
 	if err != nil {
-		return "", fmt.Errorf("invalid source name %s: %v", imgRef, err)
+		return "", fmt.Errorf("invalid source name %s: %w", imgRef, err)
 	}
 
 	img, err := srcRef.NewImageSource(ctx, sourceCtx)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("new image source: %w", err)
 	}
+	defer img.Close()
 
 	manifestBytes, _, err := img.GetManifest(ctx, nil)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("get manifest: %w", err)
 	}
 
 	digest, err := manifest.Digest(manifestBytes)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("get manifest digest: %w", err)
 	}
 
-	var digestString string
-	if strings.Contains(digest.String(), ":") {
-		digestString = strings.Split(digest.String(), ":")[1]
-	}
-
-	return digestString, nil
-}
-
-func setInternalLog(log clog.PluggableLoggerInterface) {
-	if internalLog == nil {
-		internalLog = log
-	}
+	return digest.Encoded(), nil
 }
--- v2/internal/pkg/manifest/oci-manifest_test.go
+++ v2/internal/pkg/manifest/oci-manifest_test.go
@@ -2,7 +2,11 @@ package manifest
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/otiai10/copy"
+	"github.com/stretchr/testify/assert"
 
 	"github.com/openshift/oc-mirror/v2/internal/pkg/api/v2alpha1"
 	"github.com/openshift/oc-mirror/v2/internal/pkg/common"
@@ -10,51 +14,140 @@ import (
 )
 
 func TestGetAllManifests(t *testing.T) {
-
 	log := clog.New("debug")
+	manifest := &Manifest{Log: log}
 
-	// these tests should cover over 80%
-	t.Run("Testing GetImageIndex : should pass", func(t *testing.T) {
-		manifest := &Manifest{Log: log}
-		res, err := manifest.GetImageIndex(common.TestFolder)
-		if err != nil {
-			t.Fatalf("should not fail")
+	// The failure cases are just json parsing errors
+	t.Run("Testing GetImageManifest : should pass", func(t *testing.T) {
+		expectedOCI := &v2alpha1.OCISchema{
+			SchemaVersion: 2,
+			MediaType:     "application/vnd.oci.image.manifest.v1+json",
+			Config: v2alpha1.OCIManifest{
+				MediaType: "application/vnd.oci.image.config.v1+json",
+				Digest:    "sha256:18249fc55c217d5ecc543fb7cae29cdcbc8a7d94691e8a684f336abf75d01e64",
+				Size:      1672,
+			},
+			Layers: []v2alpha1.OCIManifest{
+				{
+					MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+					Digest:    "sha256:97da74cc6d8fa5d1634eb1760fd1da5c6048619c264c23e62d75f3bf6b8ef5c4",
+					Size:      79524639,
+				},
+				{
+					MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+					Digest:    "sha256:d8190195889efb5333eeec18af9b6c82313edd4db62989bd3a357caca4f13f0e",
+					Size:      1438,
+				},
+				{
+					MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+					Digest:    "sha256:f0f4937bc70fa7bf9afc1eb58400dbc646c9fd0c9f95cfdbfcdedd55f6fa0bcd",
+					Size:      26654429,
+				},
+				{
+					MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+					Digest:    "sha256:833de2b0ccff7a77c31b4d2e3f96077b638aada72bfde75b5eddd5903dc11bb7",
+					Size:      12374694,
+				},
+				{
+					MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+					Digest:    "sha256:911c7f3bfc1ca79614a05b77ad8b28e87f71026d41a34c8ea14b4f0a3657d0eb",
+					Size:      25467095,
+				},
+				{
+					MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+					Digest:    "sha256:5b2ca04f694b70c8b41f1c2a40b7e95643181a1d037b115149ecc243324c513d",
+					Size:      955593,
+				},
+			},
 		}
-		log.Debug("completed test  %v ", res)
+		res, err := manifest.GetImageManifest(filepath.Join(common.TestFolder, "image-manifest.json"))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedOCI, res)
 	})
 
-	t.Run("Testing GetImageManifest : should pass", func(t *testing.T) {
-		manifest := &Manifest{Log: log}
-		res, err := manifest.GetImageManifest(common.TestFolder + "image-manifest.json")
-		if err != nil {
-			t.Fatalf("should not fail")
+	// The failing cases are already covered by GetImageManifest tests
+	t.Run("Testing GetImageIndex : should pass", func(t *testing.T) {
+		expectedOCI := &v2alpha1.OCISchema{
+			SchemaVersion: 2,
+			Manifests: []v2alpha1.OCIManifest{
+				{
+					MediaType: "application/vnd.oci.image.manifest.v1+json",
+					Digest:    "sha256:3ad31ff3302d352771aba8d1262e2d87cd4f796eac7195401c4d9c9af64e1627",
+					Size:      1196,
+				},
+			},
 		}
-		log.Debug("completed test  %v ", res)
+		res, err := manifest.GetImageIndex(common.TestFolder)
+		assert.NoError(t, err)
+		assert.Equal(t, expectedOCI, res)
 	})
 
 	t.Run("Testing GetOperatorConfig : should pass", func(t *testing.T) {
-		manifest := &Manifest{Log: log}
-		res, err := manifest.GetOperatorConfig(common.TestFolder + "operator-config.json")
-		if err != nil {
-			t.Fatalf("should not fail")
+		expectedConfig := &v2alpha1.OperatorConfigSchema{
+			Architecture: "amd64",
+			Os:           "linux",
+			Config: v2alpha1.OperatorConfig{
+				Env: []string{
+					"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+					"container=oci",
+					"GODEBUG=x509ignoreCN=0,madvdontneed=1",
+					"__doozer=merge",
+					"BUILD_RELEASE=202301042354.p0.gf1dc3b6.assembly.stream",
+					"BUILD_VERSION=v4.12.0",
+					"OS_GIT_MAJOR=4",
+					"OS_GIT_MINOR=12",
+					"OS_GIT_PATCH=0",
+					"OS_GIT_TREE_STATE=clean",
+					"OS_GIT_VERSION=4.12.0-202301042354.p0.gf1dc3b6.assembly.stream-f1dc3b6",
+					"SOURCE_GIT_TREE_STATE=clean",
+					"OS_GIT_COMMIT=f1dc3b6",
+					"SOURCE_DATE_EPOCH=1668720573",
+					"SOURCE_GIT_COMMIT=f1dc3b6a6b7c5f5a85f94201ab90f9e03547a8a3",
+					"SOURCE_GIT_TAG=v1.0.0-930-gf1dc3b6a",
+					"SOURCE_GIT_URL=https://github.com/openshift/cluster-version-operator",
+				},
+				Entrypoint: []string{"/usr/bin/cluster-version-operator"},
+			},
+			RootFS: v2alpha1.OperatorRootFS{
+				Type: "layers",
+				DiffIds: []string{
+					"sha256:e2e51ecd22dcbc318fb317f20dff685c6d54755d60a80b12ed290658864d45fd",
+					"sha256:d3fbfed1573def1cd078186e307411a8929138baf65bdd0a02bcbdb451707f67",
+					"sha256:10f5b8a5bcc334c27cb0c778c545ce6c3c77c79c9300e3bd5574dc7b12ef1e44",
+					"sha256:50c36aad8a28245871d2cace944a4260d9de58b679a0e4ab285b481e123734eb",
+					"sha256:8125f4f93a05cc066bb1660a63fcc1da73cf2fe10db2f512c79434e5c4108217",
+					"sha256:eab47dc5c5fd4c2ad223788f24e27c437c95099c0e2b93cebf188347e1de9912",
+				},
+			},
 		}
-		log.Debug("completed test  %v ", res)
+		res, err := manifest.GetOperatorConfig(filepath.Join(common.TestFolder, "operator-config.json"))
+		assert.NoError(t, err)
+		// We don't care about some fields from the Schema (e.g. History), so
+		// we cannot just compare the objects. Instead, we compare just the relevant fields
+		// assert.Equal(t, expectedConfig, res)
+		assert.Equal(t, expectedConfig.Config, res.Config, ".Config should match")
+		assert.Equal(t, expectedConfig.RootFS, res.RootFS, ".RootFS should match")
+		assert.Equal(t, expectedConfig.Architecture, res.Architecture, ".Architecture should match")
+		assert.Equal(t, expectedConfig.Os, res.Os, ".Os should match")
 	})
 
 	t.Run("Testing GetReleaseSchema : should pass", func(t *testing.T) {
-		manifest := &Manifest{Log: log}
-		res, err := manifest.GetReleaseSchema(common.TestFolder + "release-schema.json")
-		if err != nil {
-			log.Error(" %v ", err)
-			t.Fatalf("should not fail")
+		expectedRI := []v2alpha1.RelatedImage{
+			{
+				Name:  "agent-installer-api-server",
+				Image: "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:f30638f60452062aba36a26ee6c036feead2f03b28f2c47f2b0a991e4182331e",
+				Type:  2,
+			},
 		}
-		log.Debug("completed test  %v ", res)
+		res, err := manifest.GetReleaseSchema(filepath.Join(common.TestFolder, "release-schema.json"))
+		assert.NoError(t, err)
+		assert.Equal(t, expectedRI, res)
 	})
 }
 
 func TestExtractOCILayers(t *testing.T) {
-
 	log := clog.New("debug")
+	manifest := &Manifest{Log: log}
 	t.Run("Testing ExtractOCILayers : should pass", func(t *testing.T) {
 		oci := &v2alpha1.OCISchema{
 			SchemaVersion: 2,
@@ -83,26 +176,120 @@ func TestExtractOCILayers(t *testing.T) {
 				},
 			},
 		}
-		manifest := &Manifest{Log: log}
-		// this should do a nop (directory exists)
-		err := manifest.ExtractLayersOCI(common.TestFolder+"test-untar/blobs/sha256", common.TestFolder+"test-untar", "release-manifests/", oci)
-		if err != nil {
-			t.Fatal("should not fail")
-		}
+		t.Run("when destination directory exists - no op", func(t *testing.T) {
+			// this should do a nop (directory exists)
+			srcDir := filepath.Join(common.TestFolder, "test-untar", "blobs", "sha256")
+			destDir := filepath.Join(common.TestFolder, "test-untar", "release-manifests")
+			assert.DirExists(t, destDir, "directory should exist as precondition")
+			err := manifest.ExtractLayersOCI(srcDir, filepath.Join(common.TestFolder, "test-untar"), "release-manifests", oci)
+			assert.NoError(t, err, "should not fail: no op")
+			assert.DirExists(t, destDir, "directory should still exist")
+		})
+		t.Run("when destination directory doesn't exist", func(t *testing.T) {
+			destDir := t.TempDir()
 
-		_, err = os.Stat(common.TestFolder + "hold-test-untar/release-manifests/")
-		if err == nil {
-			t.Fatalf("should fail")
-		}
+			err := manifest.ExtractLayersOCI(filepath.Join(common.TestFolder, "test-untar", "blobs", "sha256"), destDir, "release-manifests", oci)
+			assert.NoError(t, err)
 
-		err = manifest.ExtractLayersOCI(common.TestFolder+"test-untar/blobs/sha256", common.TestFolder+"hold-test-untar", "release-manifests/", oci)
-		if err != nil {
-			t.Fatalf("should not fail")
-		}
-		defer os.RemoveAll(common.TestFolder + "hold-test-untar")
-		_, err = os.Stat(common.TestFolder + "hold-test-untar/release-manifests/")
-		if err != nil {
-			t.Fatalf("should not fail")
-		}
+			manifestsDir := filepath.Join(destDir, "release-manifests")
+			f, err := os.Open(manifestsDir)
+			assert.NoError(t, err, "release-manifests dir should exist")
+			defer f.Close()
+
+			// We assume we are good if the file count matches
+			const expectedManifestCount = 646
+			flist, err := f.Readdirnames(-1)
+			assert.NoError(t, err, "should read release-manifests contents")
+			assert.Equal(t, expectedManifestCount, len(flist), "manifest count mismatch")
+		})
+	})
+	t.Run("Testing ExtractOCILayers : should fail", func(t *testing.T) {
+		t.Run("when cannot stat extract destination directory", func(t *testing.T) {
+			// Create a directory we don't have permission to stat
+			destDir := filepath.Join(t.TempDir(), "invalid-dest")
+			err := os.Mkdir(destDir, 0o600)
+			assert.NoError(t, err)
+
+			oci := &v2alpha1.OCISchema{}
+			err = manifest.ExtractLayersOCI(filepath.Join(common.TestFolder, "test-untar", "blobs", "sha256"), destDir, "release-manifests", oci)
+			assert.Error(t, err)
+			assert.Regexp(t, "extract directory: .*", err)
+		})
+		t.Run("when OCI layer digests are malformed", func(t *testing.T) {
+			oci := &v2alpha1.OCISchema{
+				SchemaVersion: 2,
+				Manifests: []v2alpha1.OCIManifest{
+					{
+						MediaType: "application/vnd.oci.image.manifest.v1+json",
+						Digest:    "sha256:3ef0b0141abd1548f60c4f3b23ecfc415142b0e842215f38e98610a3b2e52419",
+						Size:      567,
+					},
+				},
+				Config: v2alpha1.OCIManifest{
+					MediaType: "application/vnd.oci.image.manifest.v1+json",
+					Digest:    "sha256:3ef0b0141abd1548f60c4f3b23ecfc415142b0e842215f38e98610a3b2e52419",
+					Size:      567,
+				},
+				Layers: []v2alpha1.OCIManifest{
+					{
+						MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+						Digest:    "sha256:d8190195889efb5333eeec18af9b6c82313edd4db62989bd3a357caca4f13f0e",
+						Size:      1438,
+					},
+					{
+						MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+
+						Digest: "foobaz",
+						Size:   955593,
+					},
+				},
+			}
+			destDir := t.TempDir()
+			err := manifest.ExtractLayersOCI(filepath.Join(common.TestFolder, "test-untar", "blobs", "sha256"), destDir, "release-manifests", oci)
+			assert.EqualError(t, err, "digest \"foobaz\": format is not correct: invalid checksum digest format")
+		})
+		t.Run("when OCI layer is missing", func(t *testing.T) {
+			oci := &v2alpha1.OCISchema{
+				SchemaVersion: 2,
+				Manifests: []v2alpha1.OCIManifest{
+					{
+						MediaType: "application/vnd.oci.image.manifest.v1+json",
+						Digest:    "sha256:3ef0b0141abd1548f60c4f3b23ecfc415142b0e842215f38e98610a3b2e52419",
+						Size:      567,
+					},
+				},
+				Config: v2alpha1.OCIManifest{
+					MediaType: "application/vnd.oci.image.manifest.v1+json",
+					Digest:    "sha256:3ef0b0141abd1548f60c4f3b23ecfc415142b0e842215f38e98610a3b2e52419",
+					Size:      567,
+				},
+				Layers: []v2alpha1.OCIManifest{
+					{
+						MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+						Digest:    "sha256:d8190195889efb5333eeec18af9b6c82313edd4db62989bd3a357caca4f13f0e",
+						Size:      1438,
+					},
+					{
+						MediaType: "application/vnd.oci.image.layer.v1.tar+gzip",
+						Digest:    "sha256:5b2ca04f694b70c8b41f1c2a40b7e95643181a1d037b115149ecc243324c513d",
+						Size:      955593,
+					},
+				},
+			}
+
+			// Copy layers...
+			srcDir := filepath.Join(t.TempDir(), "test-untar", "blobs", "sha256")
+			err := copy.Copy(filepath.Join(common.TestFolder, "test-untar", "blobs", "sha256"), srcDir)
+			assert.NoError(t, err, "should copy blobs")
+
+			// ... but remove one of them
+			err = os.Remove(filepath.Join(srcDir, "5b2ca04f694b70c8b41f1c2a40b7e95643181a1d037b115149ecc243324c513d"))
+			assert.NoError(t, err, "should remove blob layer")
+
+			destDir := t.TempDir()
+			err = manifest.ExtractLayersOCI(srcDir, destDir, "release-manifests", oci)
+			assert.Error(t, err)
+			assert.Regexp(t, "digest \"5b2ca04f694b70c8b41f1c2a40b7e95643181a1d037b115149ecc243324c513d\": open origin layer: open .*: no such file or directory", err)
+		})
 	})
 }
