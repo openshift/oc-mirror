6e55f4015b507a65e0f4a262b9fe78cd8e0f623a
Rafael F. <r4f4rfs@gmail.com>
2025-03-07 17:03:35 +0100

    OCPBUGS-50680: v2/operator: refactor operator FilteredCollector (#1093)
    * v2/operator/common: dedup code for catalogDigest
    
    I have also added a unit test for `cachedCatalog`.
    
    * v2/api: prepare CatalogFilterResult for refactor
    
    We will need this information so we can refactor the OperatorCollector.
    
    * v2/operator: refactor FilteredCollector
    
    Instead of an endless chain of if-else, let's separate blocks into
    function according to their actions.
    
    This makes the collector easier to read, remove error handling
    duplication (like aborting the spinner), and it will make easier to skip
    operators but return a list of failed operators at the end.
    
    * v2/operator: skip failed operators
    
    If we fail to collect an operator, save the error but keep collecting
    the other operators on the list. At the end, we return all errors found.
    
    Further work is need to not stop at collection and proceed to the copy
    but still return non-zero exit status at the end of oc-mirror execution.
    
    * v2/operator: copy OCI operator on any errors
    
    Before we were just copying the operator contents to the working dir if
    the `stat` call returned `ErrNotExist`. To be safe, we should try to
    copy on any errors.
    
    * v2/operator: fix linting issues
    
    We'll have to postpone fixing the cyclop issues until we are able to
    refactor the code even further.

--- v2/internal/pkg/operator/common.go
+++ v2/internal/pkg/operator/common.go
@@ -5,15 +5,20 @@ import (
 	"fmt"
 	"hash/fnv"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/containers/image/v5/types"
+	"github.com/opencontainers/go-digest"
+
 	"github.com/openshift/oc-mirror/v2/internal/pkg/api/v2alpha1"
 	"github.com/openshift/oc-mirror/v2/internal/pkg/image"
 	clog "github.com/openshift/oc-mirror/v2/internal/pkg/log"
 	"github.com/openshift/oc-mirror/v2/internal/pkg/manifest"
 	"github.com/openshift/oc-mirror/v2/internal/pkg/mirror"
 )
+
+const latestTag string = "latest"
 
 type OperatorCollector struct {
 	Log                clog.PluggableLoggerInterface
@@ -58,23 +63,25 @@ func isMultiManifestIndex(oci v2alpha1.OCISchema) bool {
 // * `catalog` (`v2alpha1.Operator`): the reference to the catalog in the imageSetConfig along with targetCatalog and targetTag if set
 // * the `filteredTag`: which is the expected tag to be used for the filtered catalog.
 func (o OperatorCollector) cachedCatalog(catalog v2alpha1.Operator, filteredTag string) (string, error) {
-	var src string
 	srcImgSpec, err := image.ParseRef(catalog.Catalog)
 	if err != nil {
-		return "", fmt.Errorf("unable to determine cached reference for catalog %s: %v", catalog.Catalog, err)
+		return "", fmt.Errorf("unable to determine cached reference for catalog: %w", err)
 	}
 
-	// prepare the src and dest references
+	// prepare the src reference
+	var src string
 	switch {
 	case len(catalog.TargetCatalog) > 0:
-		src = dockerProtocol + strings.Join([]string{o.LocalStorageFQDN, catalog.TargetCatalog}, "/")
+		src = fmt.Sprintf("%s%s/%s", dockerProtocol, o.LocalStorageFQDN, catalog.TargetCatalog)
 	case srcImgSpec.Transport == ociProtocol:
-		src = dockerProtocol + strings.Join([]string{o.LocalStorageFQDN, path.Base(srcImgSpec.Reference)}, "/")
+		src = fmt.Sprintf("%s%s/%s", dockerProtocol, o.LocalStorageFQDN, path.Base(srcImgSpec.Reference))
 	default:
-		src = dockerProtocol + strings.Join([]string{o.LocalStorageFQDN, srcImgSpec.PathComponent}, "/")
+		src = fmt.Sprintf("%s%s/%s", dockerProtocol, o.LocalStorageFQDN, srcImgSpec.PathComponent)
 	}
 
-	src = src + ":" + filteredTag
+	if len(filteredTag) > 0 {
+		return src + ":" + filteredTag, nil
+	}
 
 	return src, nil
 }
@@ -82,37 +89,30 @@ func (o OperatorCollector) cachedCatalog(catalog v2alpha1.Operator, filteredTag 
 // catalogDigest: method used during diskToMirror in order to discover the catalog's digest from a reference by tag.
 // It queries the cache registry instead of the registry set in the `catalog` reference
 func (o OperatorCollector) catalogDigest(ctx context.Context, catalog v2alpha1.Operator) (string, error) {
-	var src string
-
 	srcImgSpec, err := image.ParseRef(catalog.Catalog)
 	if err != nil {
-		return "", fmt.Errorf("unable to determine cached reference for catalog %s: %v", catalog.Catalog, err)
+		return "", fmt.Errorf("unable to determine cached reference for catalog: %w", err)
 	}
 
-	// prepare the src and dest references
-	switch {
-	case len(catalog.TargetCatalog) > 0:
-		src = dockerProtocol + strings.Join([]string{o.LocalStorageFQDN, catalog.TargetCatalog}, "/")
-	case srcImgSpec.Transport == ociProtocol:
-		src = dockerProtocol + strings.Join([]string{o.LocalStorageFQDN, path.Base(srcImgSpec.Reference)}, "/")
-	default:
-		src = dockerProtocol + strings.Join([]string{o.LocalStorageFQDN, srcImgSpec.PathComponent}, "/")
-	}
-
+	var tag string
 	switch {
 	case len(catalog.TargetTag) > 0: // applies only to catalogs
-		src = src + ":" + catalog.TargetTag
+		tag = catalog.TargetTag
 	case srcImgSpec.Tag == "" && srcImgSpec.Digest != "":
-		src = src + ":" + srcImgSpec.Algorithm + "-" + srcImgSpec.Digest
+		tag = fmt.Sprintf("%s-%s", srcImgSpec.Algorithm, srcImgSpec.Digest)
 	case srcImgSpec.Tag == "" && srcImgSpec.Digest == "" && srcImgSpec.Transport == ociProtocol:
-		src = src + ":latest"
+		tag = latestTag
 	default:
-		src = src + ":" + srcImgSpec.Tag
+		tag = srcImgSpec.Tag
+	}
+
+	src, err := o.cachedCatalog(catalog, tag)
+	if err != nil {
+		return "", err
 	}
 
 	imgSpec, err := image.ParseRef(src)
 	if err != nil {
-		o.Log.Error(errMsg, err.Error())
 		return "", err
 	}
 
@@ -125,12 +125,7 @@ func (o OperatorCollector) catalogDigest(ctx context.Context, catalog v2alpha1.O
 		sourceCtx.DockerInsecureSkipTLSVerify = types.OptionalBoolTrue
 	}
 
-	catalogDigest, err := o.Manifest.GetDigest(ctx, sourceCtx, imgSpec.ReferenceWithTransport)
-	if err != nil {
-		o.Log.Error(errMsg, err.Error())
-		return "", err
-	}
-	return catalogDigest, nil
+	return o.Manifest.GetDigest(ctx, sourceCtx, imgSpec.ReferenceWithTransport)
 }
 
 func (o OperatorCollector) prepareD2MCopyBatch(images map[string][]v2alpha1.RelatedImage) ([]v2alpha1.CopyImageSchema, error) {
@@ -181,7 +176,7 @@ func (o OperatorCollector) prepareD2MCopyBatch(images map[string][]v2alpha1.Rela
 				} else {
 					src = src + ":" + imgSpec.Algorithm + "-" + imgSpec.Digest
 				}
-				//TODO remove me when the migration from oc-mirror v1 to v2 ends
+				// TODO remove me when the migration from oc-mirror v1 to v2 ends
 				if o.generateV1DestTags {
 					if img.OriginFromOperatorCatalogOnDisk {
 						dest = dest + ":" + imgSpec.Digest[0:6]
@@ -258,7 +253,7 @@ func (o OperatorCollector) prepareM2DCopyBatch(images map[string][]v2alpha1.Rela
 			case img.Type == v2alpha1.TypeOperatorCatalog && len(img.TargetTag) > 0:
 				dest = dest + ":" + img.TargetTag
 			case imgSpec.Tag == "" && imgSpec.Transport == ociProtocol:
-				dest = dest + "::latest"
+				dest = dest + "::" + latestTag
 			case imgSpec.IsImageByDigestOnly():
 				dest = dest + ":" + imgSpec.Algorithm + "-" + imgSpec.Digest
 			case imgSpec.IsImageByTagAndDigest(): // OCPBUGS-33196 + OCPBUGS-37867- check source image for tag and digest
@@ -334,7 +329,91 @@ func (o OperatorCollector) dispatchImagesForM2M(images map[string][]v2alpha1.Rel
 		}
 	}
 	return result, nil
+}
 
+func (o OperatorCollector) extractOCIConfigLayers(catalog string, imgSpec image.ImageSpec, imageIndexDir string) (string, error) { //nolint:cyclop // TODO: this needs further refactoring
+	o.Log.Debug("Extracting OCI catalog layers")
+	configsDir := filepath.Join(imageIndexDir, operatorCatalogConfigDir)
+	catalogImageDir := filepath.Join(imageIndexDir, operatorCatalogImageDir)
+
+	if err := createFolders([]string{configsDir, catalogImageDir}); err != nil {
+		return "", err
+	}
+
+	// It's in oci format so we can go directly to the index.json file
+	oci, err := o.Manifest.GetImageIndex(catalogImageDir)
+	if err != nil {
+		return "", err
+	}
+
+	if isMultiManifestIndex(*oci) && imgSpec.Transport == ociProtocol {
+		if err := o.Manifest.ConvertIndexToSingleManifest(catalogImageDir, oci); err != nil {
+			return "", err
+		}
+
+		var err error
+		oci, err = o.Manifest.GetImageIndex(catalogImageDir)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	if len(oci.Manifests) == 0 {
+		return "", fmt.Errorf("no manifests found for %s", catalog)
+	}
+
+	validDigest, err := digest.Parse(oci.Manifests[0].Digest)
+	if err != nil {
+		return "", fmt.Errorf("the digests seem to be incorrect for %s: %w", catalog, err)
+	}
+
+	manifest := validDigest.Encoded()
+	o.Log.Debug(collectorPrefix+"manifest %s", manifest)
+	manifestDir := filepath.Join(catalogImageDir, blobsDir, manifest)
+	oci, err = o.Manifest.GetImageManifest(manifestDir)
+	if err != nil {
+		return "", err
+	}
+
+	// we need to check if oci returns multi manifests (from manifest list)
+	// also oci.Config will be nil
+	// we are only interested in the first manifest as all architectures
+	// "configs" will be exactly the same
+	if len(oci.Manifests) > 1 && oci.Config.Size == 0 {
+		subDigest, err := digest.Parse(oci.Manifests[0].Digest)
+		if err != nil {
+			return "", fmt.Errorf("the digests seem to be incorrect for %s: %w", catalog, err)
+		}
+		manifestDir := filepath.Join(catalogImageDir, blobsDir, subDigest.Encoded())
+		oci, err = o.Manifest.GetImageManifest(manifestDir)
+		if err != nil {
+			return "", fmt.Errorf("manifest %s: %w", catalog, err)
+		}
+	}
+
+	// read the config digest to get the detailed manifest
+	// looking for the label to search for a specific folder
+	configDigest, err := digest.Parse(oci.Config.Digest)
+	if err != nil {
+		return "", fmt.Errorf("the digests seem to be incorrect for %s: %w", catalog, err)
+	}
+	catalogDir := filepath.Join(catalogImageDir, blobsDir, configDigest.Encoded())
+	ocs, err := o.Manifest.GetOperatorConfig(catalogDir)
+	if err != nil {
+		return "", err
+	}
+
+	label := ocs.Config.Labels.OperatorsOperatorframeworkIoIndexConfigsV1
+	o.Log.Debug(collectorPrefix+"label %q", label)
+
+	// untar all the blobs for the operator
+	// if the layer with "label" (from previous step) is found to a specific folder
+	fromDir := filepath.Join(catalogImageDir, blobsDir)
+	if err := o.Manifest.ExtractLayersOCI(fromDir, configsDir, label, oci); err != nil {
+		return "", err
+	}
+
+	return filepath.Join(configsDir, label), nil
 }
 
 type OtherImageDispatcher struct {
@@ -356,7 +435,7 @@ func (d OtherImageDispatcher) dispatch(img v2alpha1.RelatedImage) ([]v2alpha1.Co
 	dest = strings.Join([]string{d.destinationRegistry, imgSpec.PathComponent}, "/")
 	switch {
 	case imgSpec.Tag == "" && imgSpec.Transport == ociProtocol:
-		dest = dest + ":latest"
+		dest = dest + ":" + latestTag
 	case imgSpec.IsImageByDigestOnly():
 		dest = dest + ":" + imgSpec.Algorithm + "-" + imgSpec.Digest
 	case imgSpec.IsImageByTagAndDigest(): // OCPBUGS-33196 + OCPBUGS-37867- check source image for tag and digest
@@ -422,7 +501,7 @@ func saveCtlgToCacheRef(spec image.ImageSpec, img v2alpha1.RelatedImage, cacheRe
 	case len(img.TargetTag) > 0:
 		saveCtlgDest = saveCtlgDest + ":" + img.TargetTag
 	case spec.Tag == "" && spec.Transport == ociProtocol:
-		saveCtlgDest = saveCtlgDest + ":latest"
+		saveCtlgDest = saveCtlgDest + ":" + latestTag
 	case spec.IsImageByDigestOnly():
 		saveCtlgDest = saveCtlgDest + ":" + spec.Algorithm + "-" + spec.Digest
 	case spec.IsImageByTagAndDigest():
@@ -455,7 +534,7 @@ func rebuiltCtlgRef(spec image.ImageSpec, img v2alpha1.RelatedImage, cacheRegist
 	case img.Type == v2alpha1.TypeOperatorCatalog && len(img.TargetTag) > 0:
 		rebuiltCtlgSrc = rebuiltCtlgSrc + ":" + img.TargetTag
 	case spec.Tag == "" && spec.Transport == ociProtocol:
-		rebuiltCtlgSrc = rebuiltCtlgSrc + ":latest"
+		rebuiltCtlgSrc = rebuiltCtlgSrc + ":" + latestTag
 	case spec.IsImageByDigestOnly():
 		rebuiltCtlgSrc = rebuiltCtlgSrc + ":" + spec.Algorithm + "-" + spec.Digest
 	case spec.IsImageByTagAndDigest(): // OCPBUGS-33196 + OCPBUGS-37867- check source image for tag and digest
@@ -487,7 +566,7 @@ func destCtlgRef(spec image.ImageSpec, img v2alpha1.RelatedImage, destinationReg
 	case len(img.TargetTag) > 0:
 		dest = dest + ":" + img.TargetTag
 	case spec.Tag == "" && spec.Transport == ociProtocol:
-		dest = dest + ":latest"
+		dest = dest + ":" + latestTag
 	case spec.IsImageByDigestOnly():
 		dest = dest + ":" + spec.Algorithm + "-" + spec.Digest
 	case spec.IsImageByTagAndDigest(): // OCPBUGS-33196 + OCPBUGS-37867- check source image for tag and digest
