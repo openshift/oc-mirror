45f1c7763c2aabff457a37ea89d841bf06d7cb3f
Rafael Fonseca <r4f4rfs@gmail.com>
2025-03-11 17:22:30 +0100

    v2/common: add exit return codes consts
    These will be useful to return exit codes according to the type of
    errors found.

--- v2/internal/pkg/common/const.go
+++ v2/internal/pkg/common/const.go
@@ -2,4 +2,10 @@ package common
 
 const (
 	TestFolder string = "../../../tests/"
+
+	GenericErrCode       = 1 << 0
+	ReleaseErrCode       = 1 << 1
+	OperatorErrCode      = 1 << 2
+	AdditionalImgErrCode = 1 << 3
+	HelmErrCode          = 1 << 4
 )
