7eed8d874011ff5db1c9a799b331ad304ff9846d
Rafael Fonseca <r4f4rfs@gmail.com>
2025-03-11 17:27:45 +0100

    v2/cli: introduce a `CodeExiter` interface
    This interface can be implemented by errors causing oc-mirror to exit
    with the specified exit code.

--- v2/internal/pkg/cli/error.go
+++ v2/internal/pkg/cli/error.go
@@ -20,3 +20,8 @@ func (e *NormalStorageInterruptError) Is(err error) bool {
 	_, ok := err.(*NormalStorageInterruptError)
 	return ok
 }
+
+// CodeExiter is an interface implemented by errors that result in an exit code
+type CodeExiter interface {
+	ExitCode() int
+}
