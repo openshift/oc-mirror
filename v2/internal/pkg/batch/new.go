fb855f46f0d1d68f793f20eacbed33be1a39f40d
Rafael Fonseca <r4f4rfs@gmail.com>
2025-03-20 16:53:37 +0100

    v2/batch: fix gosec and ireturn linter issues
    `MaxGoroutines` is not supposed to be negative, so let's use `uint`
    instead of `int` and avoid the `uint -> int` conversion altogether.

--- v2/internal/pkg/batch/new.go
+++ v2/internal/pkg/batch/new.go
@@ -5,11 +5,15 @@ import (
 	"github.com/openshift/oc-mirror/v2/internal/pkg/mirror"
 )
 
+// We want to return an interface here since `New` is a convenience function to
+// easily swap out batch worker implementations in the callee.
+//
+//nolint:ireturn
 func New(workerType string,
 	log clog.PluggableLoggerInterface,
 	logsDir string,
 	mirror mirror.MirrorInterface,
 	batchSize uint,
 ) BatchInterface {
-	return &ChannelConcurrentBatch{Log: log, LogsDir: logsDir, Mirror: mirror, MaxGoroutines: int(batchSize)}
+	return &ChannelConcurrentBatch{Log: log, LogsDir: logsDir, Mirror: mirror, MaxGoroutines: batchSize}
 }
