c7e1955f6ae8c99b8514c5380bb55e5e60c531b3
Luigi Mario Zuccarelli <luzuccar@redhat.com>
2025-04-24 08:36:36 +0200

    Update from review

--- v2/internal/pkg/operator/filtered_collector.go
+++ v2/internal/pkg/operator/filtered_collector.go
@@ -264,7 +264,7 @@ func (o FilterCollector) collectOperator( //nolint:cyclop // TODO: this needs fu
 	if imgSpec.IsImageByDigestOnly() && o.Opts.IsMirrorToDisk() {
 		tag, err := digestOfFilter(op)
 		if err != nil {
-			return v2alpha1.CatalogFilterResult{}, err
+			return v2alpha1.CatalogFilterResult{}, fmt.Errorf("failed to get filter digest for operator mirrored by digest: %w", err)
 		}
 		componentName = imgSpec.ComponentName() + "." + tag
 	}
@@ -374,6 +374,7 @@ func (o FilterCollector) filterOperator(ctx context.Context, op v2alpha1.Operato
 	}
 
 	filteredDigestPath := filepath.Join(filteredCatalogsDir, filterDigest, operatorCatalogConfigDir)
+
 	if err := createFolders([]string{filteredDigestPath}); err != nil {
 		return v2alpha1.CatalogFilterResult{}, err
 	}
