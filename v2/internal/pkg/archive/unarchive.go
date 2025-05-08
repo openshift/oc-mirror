26bbaf995967d6af7eee46b63c3dd8d8824348bf
Rafael Fonseca <r4f4rfs@gmail.com>
2025-05-08 21:42:08 +0200

    v2/archive: small refactoring of the Unarchive code

--- v2/internal/pkg/archive/unarchive.go
+++ v2/internal/pkg/archive/unarchive.go
@@ -33,7 +33,6 @@ func NewArchiveExtractor(archivePath, workingDir, cacheDir string) (MirrorUnArch
 		return MirrorUnArchiver{}, err
 	}
 	for _, chunk := range files {
-
 		if rxp.MatchString(chunk.Name()) {
 			ae.archiveFiles = append(ae.archiveFiles, filepath.Join(archivePath, chunk.Name()))
 		}
@@ -45,82 +44,96 @@ func NewArchiveExtractor(archivePath, workingDir, cacheDir string) (MirrorUnArch
 // * docker/v2* to cacheDir
 // * working-dir to workingDir
 func (o MirrorUnArchiver) Unarchive() error {
+	// make sure workingDir exists
+	if err := os.MkdirAll(o.workingDir, 0755); err != nil {
+		return fmt.Errorf("unable to create working dir %q: %w", o.workingDir, err)
+	}
+	// make sure cacheDir exists
+	if err := os.MkdirAll(o.cacheDir, 0755); err != nil {
+		return fmt.Errorf("unable to create cache dir %q: %w", o.cacheDir, err)
+	}
+
 	for _, chunkPath := range o.archiveFiles {
-		chunkFile, err := os.Open(chunkPath)
-		if err != nil {
+		if err := o.unarchiveChunkTarFile(chunkPath); err != nil {
 			return err
 		}
-		defer chunkFile.Close()
-		reader := tar.NewReader(chunkFile)
-		// make sure workingDir exists
-		err = os.MkdirAll(o.workingDir, 0755)
+	}
+
+	return nil
+}
+
+func (o MirrorUnArchiver) unarchiveChunkTarFile(chunkPath string) error {
+	chunkFile, err := os.Open(chunkPath)
+	if err != nil {
+		return fmt.Errorf("unable to open chunk tar file: %w", err)
+	}
+	defer chunkFile.Close()
+	workingDirParent := filepath.Dir(o.workingDir)
+	reader := tar.NewReader(chunkFile)
+	for {
+		header, err := reader.Next()
+
+		// break the infinite loop when EOF
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
 		if err != nil {
-			return fmt.Errorf(errMessageFolder, o.workingDir, err)
+			return fmt.Errorf("error reading archive %s: %w", chunkFile.Name(), err)
 		}
-		// make sure cacheDir exists
-		err = os.MkdirAll(o.cacheDir, 0755)
-		if err != nil {
-			return fmt.Errorf(errMessageFolder, o.cacheDir, err)
+
+		if header == nil {
+			continue
 		}
-		for {
-			header, err := reader.Next()
 
-			// break the infinite loop when EOF
-			if errors.Is(err, io.EOF) {
-				break
-			}
-
-			if err != nil {
-				return fmt.Errorf("error reading archive %s: %v", chunkFile.Name(), err)
-			}
-
-			if header == nil {
-				continue
-			}
-			// taking only files into account
-			// because we are considering that all parent folders will be
-			// created recursively, and that, to the best of our knowledge
-			// the archive doesn't include any symbolic links
-
-			// for the moment we ignore imageSetConfig that is
-			// included in the tar
-			// as well as any other files that are not
-			// working-dir or cache
-
-			if header.Typeflag == tar.TypeReg {
-				descriptor := ""
-				// case file belongs to working-dir
-				if strings.Contains(header.Name, workingDirectory) {
-					workingDirParent := filepath.Dir(o.workingDir)
-					descriptor = filepath.Join(workingDirParent, header.Name)
-				} else if strings.Contains(header.Name, cacheFilePrefix) {
-					// case file belongs to the cache
-					descriptor = filepath.Join(o.cacheDir, header.Name)
-				} else {
-					continue
-				}
-				// make sure all the parent directories exist
-				descriptorParent := filepath.Dir(descriptor)
-				if err := os.MkdirAll(descriptorParent, 0755); err != nil {
-					return fmt.Errorf(errMessageFolder, descriptorParent, err)
-				}
-				// if it's a file create it, making sure it's at least writable and executable by the user
-				// since with every UnArchive, we should be able to rewrite the file
-				f, err := os.OpenFile(descriptor, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode)|0755)
-				if err != nil {
-					return fmt.Errorf("unable to create file %s: %v", descriptor, err)
-				}
-				// copy  contents
-				if _, err := io.Copy(f, reader); err != nil {
-					return fmt.Errorf("error copying file %s: %v", descriptor, err)
-				}
-
-				// manually close here after each file operation; defering would cause each file close
-				// to wait until all operations have completed.
-				f.Close()
-
-			}
+		// taking only files into account because we are considering that all
+		// parent folders will be created recursively, and that, to the best of
+		// our knowledge the archive doesn't include any symbolic links
+		if header.Typeflag != tar.TypeReg {
+			continue
 		}
+
+		// for the moment we ignore imageSetConfig that is included in the tar
+		// as well as any other files that are not working-dir or cache
+
+		descriptor := ""
+		// case file belongs to working-dir
+		if strings.Contains(header.Name, workingDirectory) {
+			workingDirParent := filepath.Dir(o.workingDir)
+			descriptor = filepath.Join(workingDirParent, header.Name)
+		} else if strings.Contains(header.Name, cacheFilePrefix) {
+			// case file belongs to the cache
+			descriptor = filepath.Join(o.cacheDir, header.Name)
+		} else {
+			continue
+		}
+		// if it's a file create it
+		// make sure it's at least writable and executable by the user
+		// since with every UnArchive, we should be able to rewrite the file
+		if err := writeFile(descriptor, reader, header.FileInfo().Mode()|0755); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeFile(filePath string, reader *tar.Reader, perm os.FileMode) error {
+	// make sure all the parent directories exist
+	descriptorParent := filepath.Dir(filePath)
+	if err := os.MkdirAll(descriptorParent, 0755); err != nil {
+		return fmt.Errorf("unable to create parent directory for %s: %w", filePath, err)
+	}
+
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, perm)
+	if err != nil {
+		return fmt.Errorf("unable to create file %s: %w", filePath, err)
+	}
+	defer f.Close()
+
+	// copy  contents
+	if _, err := io.Copy(f, reader); err != nil {
+		return fmt.Errorf("error copying file %s: %w", filePath, err)
 	}
 
 	return nil
--- v2/internal/pkg/archive/unarchive_test.go
+++ v2/internal/pkg/archive/unarchive_test.go
@@ -53,11 +53,9 @@ func TestUnArchiver_UnArchive(t *testing.T) {
 		}
 		assert.DirExists(t, filepath.Join(testFolder, "dst", "working-dir"))
 		assert.DirExists(t, filepath.Join(testFolder, "dst", "cache-dir"))
-
 	})
 
 	t.Run("unarchive with 1 archive: should pass", func(t *testing.T) {
-
 		testFolder := t.TempDir()
 		defer os.RemoveAll(testFolder)
 
@@ -91,8 +89,9 @@ func TestUnArchiver_UnArchive(t *testing.T) {
 func TestUnArchiver_NoArchive(t *testing.T) {
 	testFolder := t.TempDir()
 	defer os.RemoveAll(testFolder)
-	o, err := NewArchiveExtractor(testFolder, "dst", "none")
-
+	workingDir := t.TempDir()
+	cacheDir := t.TempDir()
+	o, err := NewArchiveExtractor(testFolder, workingDir, cacheDir)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -120,7 +119,7 @@ func TestUnArchiver_WorkingDirError(t *testing.T) {
 		t.Fatal(err)
 	}
 	err = o.Unarchive()
-	assert.Equal(t, "unable to create folder /dst: mkdir /dst: permission denied", err.Error())
+	assert.Equal(t, "unable to create working dir \"/dst\": mkdir /dst: permission denied", err.Error())
 }
 
 func TestUnArchiver_CacheDirError(t *testing.T) {
@@ -141,7 +140,7 @@ func TestUnArchiver_CacheDirError(t *testing.T) {
 		t.Fatal(err)
 	}
 	err = o.Unarchive()
-	assert.Equal(t, "unable to create folder /dst: mkdir /dst: permission denied", err.Error())
+	assert.Equal(t, "unable to create cache dir \"/dst\": mkdir /dst: permission denied", err.Error())
 }
 
 func prepareFakeTarWorkingDir(tarFile *os.File) error {
@@ -243,8 +242,8 @@ func prepareFakeTarCacheDir(tarFile *os.File) error {
 	}
 	tarWriter.Close()
 	return nil
-
 }
+
 func prepareFakeTar(tarFile *os.File) error {
 	err := prepareFakeTarWorkingDir(tarFile)
 	if err != nil {
@@ -252,5 +251,4 @@ func prepareFakeTar(tarFile *os.File) error {
 	}
 	err = prepareFakeTarCacheDir(tarFile)
 	return err
-
 }
