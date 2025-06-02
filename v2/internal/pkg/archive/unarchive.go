1b2084872f25a83e88ea1586afa0b5bda01c407c
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-06-02 10:42:29 +0200

    [release-4.18] OCPBUGS-56659: v2/archive: print unarchive progress (#1160)
    * v2/archive: small refactoring of the Unarchive code
    
    * v2/archive: sanitize path against traversal attacks
    
    This fixes the `gosec` rule:
    ```
    G305 - File path traversal when extracting zip archives
    ```
    by checking the filepaths from the archives before writing them.
    
    * OCPBUGS-55966: v2/archive: print extraction progress
    
    Populating the cache registry takes 2 hours for 300GBs of container
    content. This means oc-mirror v2 has no output on the CLI for 2 hours.
    Users see this as a stuck process.
    
    ---------
    
    Co-authored-by: Rafael Fonseca <r4f4rfs@gmail.com>

--- v2/internal/pkg/archive/unarchive.go
+++ v2/internal/pkg/archive/unarchive.go
@@ -9,6 +9,10 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
+	"golang.org/x/term"
 )
 
 type MirrorUnArchiver struct {
@@ -33,7 +37,6 @@ func NewArchiveExtractor(archivePath, workingDir, cacheDir string) (MirrorUnArch
 		return MirrorUnArchiver{}, err
 	}
 	for _, chunk := range files {
-
 		if rxp.MatchString(chunk.Name()) {
 			ae.archiveFiles = append(ae.archiveFiles, filepath.Join(archivePath, chunk.Name()))
 		}
@@ -45,82 +48,136 @@ func NewArchiveExtractor(archivePath, workingDir, cacheDir string) (MirrorUnArch
 // * docker/v2* to cacheDir
 // * working-dir to workingDir
 func (o MirrorUnArchiver) Unarchive() error {
-	for _, chunkPath := range o.archiveFiles {
-		chunkFile, err := os.Open(chunkPath)
-		if err != nil {
+	// make sure workingDir exists
+	if err := os.MkdirAll(o.workingDir, 0755); err != nil {
+		return fmt.Errorf("unable to create working dir %q: %w", o.workingDir, err)
+	}
+	// make sure cacheDir exists
+	if err := os.MkdirAll(o.cacheDir, 0755); err != nil {
+		return fmt.Errorf("unable to create cache dir %q: %w", o.cacheDir, err)
+	}
+
+	isTerminal := term.IsTerminal(int(os.Stdout.Fd()))
+	p := mpb.New(mpb.PopCompletedMode())
+	for i, chunkPath := range o.archiveFiles {
+		if !isTerminal {
+			// FIXME: replace this by a proper log call
+			fmt.Printf("Extracting chunk file (%d / %d): %s\n", i+1, len(o.archiveFiles), chunkPath)
+		}
+		stat, _ := os.Stat(chunkPath)
+		bar := p.AddBar(stat.Size(),
+			mpb.PrependDecorators(
+				decor.Name(chunkPath+" "),
+				decor.Counters(decor.SizeB1024(0), "(% .1f / % .1f)"),
+			),
+			mpb.AppendDecorators(decor.Elapsed(decor.ET_STYLE_GO)),
+		)
+
+		if err := o.unarchiveChunkTarFile(chunkPath, bar); err != nil {
+			bar.Abort(false)
+			bar.Wait()
 			return err
 		}
-		defer chunkFile.Close()
-		reader := tar.NewReader(chunkFile)
-		// make sure workingDir exists
-		err = os.MkdirAll(o.workingDir, 0755)
+		// Force completion since we can skip extracting some content from the archive
+		bar.SetCurrent(stat.Size())
+	}
+	p.Wait()
+
+	return nil
+}
+
+func (o MirrorUnArchiver) unarchiveChunkTarFile(chunkPath string, bar *mpb.Bar) error { //nolint:cyclop // cc of 11 is fine for this
+	chunkFile, err := os.Open(chunkPath)
+	if err != nil {
+		return fmt.Errorf("unable to open chunk tar file: %w", err)
+	}
+	defer chunkFile.Close()
+	workingDirParent := filepath.Dir(o.workingDir)
+	reader := tar.NewReader(chunkFile)
+	for {
+		header, err := reader.Next()
+
+		// break the infinite loop when EOF
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
 		if err != nil {
-			return fmt.Errorf(errMessageFolder, o.workingDir, err)
+			return fmt.Errorf("error reading archive %s: %w", chunkFile.Name(), err)
 		}
-		// make sure cacheDir exists
-		err = os.MkdirAll(o.cacheDir, 0755)
-		if err != nil {
-			return fmt.Errorf(errMessageFolder, o.cacheDir, err)
+
+		if header == nil {
+			continue
 		}
-		for {
-			header, err := reader.Next()
 
-			// break the infinite loop when EOF
-			if errors.Is(err, io.EOF) {
-				break
-			}
-
-			if err != nil {
-				return fmt.Errorf("error reading archive %s: %v", chunkFile.Name(), err)
-			}
-
-			if header == nil {
-				continue
-			}
-			// taking only files into account
-			// because we are considering that all parent folders will be
-			// created recursively, and that, to the best of our knowledge
-			// the archive doesn't include any symbolic links
-
-			// for the moment we ignore imageSetConfig that is
-			// included in the tar
-			// as well as any other files that are not
-			// working-dir or cache
-
-			if header.Typeflag == tar.TypeReg {
-				descriptor := ""
-				// case file belongs to working-dir
-				if strings.Contains(header.Name, workingDirectory) {
-					workingDirParent := filepath.Dir(o.workingDir)
-					descriptor = filepath.Join(workingDirParent, header.Name)
-				} else if strings.Contains(header.Name, cacheFilePrefix) {
-					// case file belongs to the cache
-					descriptor = filepath.Join(o.cacheDir, header.Name)
-				} else {
-					continue
-				}
-				// make sure all the parent directories exist
-				descriptorParent := filepath.Dir(descriptor)
-				if err := os.MkdirAll(descriptorParent, 0755); err != nil {
-					return fmt.Errorf(errMessageFolder, descriptorParent, err)
-				}
-				// if it's a file create it, making sure it's at least writable and executable by the user
-				// since with every UnArchive, we should be able to rewrite the file
-				f, err := os.OpenFile(descriptor, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode)|0755)
-				if err != nil {
-					return fmt.Errorf("unable to create file %s: %v", descriptor, err)
-				}
-				// copy  contents
-				if _, err := io.Copy(f, reader); err != nil {
-					return fmt.Errorf("error copying file %s: %v", descriptor, err)
-				}
-
-				// manually close here after each file operation; defering would cause each file close
-				// to wait until all operations have completed.
-				f.Close()
-
-			}
+		// taking only files into account because we are considering that all
+		// parent folders will be created recursively, and that, to the best of
+		// our knowledge the archive doesn't include any symbolic links
+		if header.Typeflag != tar.TypeReg {
+			continue
 		}
+
+		// for the moment we ignore imageSetConfig that is included in the tar
+		// as well as any other files that are not working-dir or cache
+
+		parentDir := ""
+		switch {
+		// case file belongs to working-dir
+		case strings.Contains(header.Name, workingDirectory):
+			parentDir = workingDirParent
+		// case file belongs to the cache
+		case strings.Contains(header.Name, cacheFilePrefix):
+			parentDir = o.cacheDir
+		default:
+			continue
+		}
+
+		// if it's a file create it
+		// make sure it's at least writable and executable by the user
+		// since with every UnArchive, we should be able to rewrite the file
+		if err := createFileWithProgress(parentDir, header, reader, bar); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// see https://github.com/securego/gosec/issues/324#issuecomment-935927967
+func sanitizeArchivePath(dir, filePath string) (string, error) {
+	v := filepath.Join(dir, filePath)
+	if strings.HasPrefix(v, filepath.Clean(dir)) {
+		return v, nil
+	}
+	return "", fmt.Errorf("content filepath is tainted: %s", filePath)
+}
+
+func createFileWithProgress(parentDir string, header *tar.Header, reader *tar.Reader, bar *mpb.Bar) error {
+	descriptor, err := sanitizeArchivePath(parentDir, header.Name)
+	if err != nil {
+		return err
+	}
+	proxyReader := bar.ProxyReader(reader)
+	defer proxyReader.Close()
+	return writeFile(descriptor, proxyReader, header.FileInfo().Mode()|0755)
+}
+
+func writeFile(filePath string, reader io.Reader, perm os.FileMode) error {
+	// make sure all the parent directories exist
+	descriptorParent := filepath.Dir(filePath)
+	if err := os.MkdirAll(descriptorParent, 0755); err != nil {
+		return fmt.Errorf("unable to create parent directory for %s: %w", filePath, err)
+	}
+
+	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_RDWR, perm)
+	if err != nil {
+		return fmt.Errorf("unable to create file %s: %w", filePath, err)
+	}
+	defer f.Close()
+
+	// copy  contents
+	if _, err := io.Copy(f, reader); err != nil {
+		return fmt.Errorf("error copying file %s: %w", filePath, err)
 	}
 
 	return nil
