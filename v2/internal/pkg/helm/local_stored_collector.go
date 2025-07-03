0d9c011e276f2b56c20bfb3f40508250c78173e7
Armel Soro <asoro@redhat.com>
2025-07-04 00:08:21 +0200

    fixup! v2: resolve helm dependencies

--- v2/internal/pkg/helm/local_stored_collector.go
+++ v2/internal/pkg/helm/local_stored_collector.go
@@ -394,8 +394,9 @@ func getHelmTemplates(ch *helmchart.Chart) (string, error) {
 	caps := chartutil.DefaultCapabilities
 
 	if err := chartutil.ProcessDependencies(ch, valueOpts); err != nil {
-		return "", fmt.Errorf("error processing dependencies: %v", err)
+		return "", fmt.Errorf("error processing dependencies: %w", err)
 	}
+
 	valuesToRender, err := chartutil.ToRenderValues(ch, valueOpts, chartutil.ReleaseOptions{}, caps)
 	if err != nil {
 		return "", fmt.Errorf("error rendering values: %w", err)
