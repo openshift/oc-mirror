353cab64b2f77141e57ca420dbe0a3150a366413
Alex Guidi <aguidi@redhat.com>
2025-08-29 18:26:56 +0200

    ocpbugs-60999: fix issue when using full true in m2m
    Before this fix during m2m when using full: true, the dispatch func was
    trying to retrieve a rebuilt catalog image from cache. This behavior was
    wrong since full: true does not rebuild the catalog.
    
    This PR fixes this behavior, it gets the original catalog image in case no RebuiltTag was found for the catalog image.

--- v2/internal/pkg/operator/filtered_collector.go
+++ v2/internal/pkg/operator/filtered_collector.go
@@ -283,6 +283,7 @@ func (o FilterCollector) collectOperator( //nolint:cyclop // TODO: this needs fu
 			TargetTag:     targetTag,
 			TargetCatalog: op.TargetCatalog,
 			RebuiltTag:    rebuiltTag,
+			FullCatalog:   isFullCatalog(op),
 		},
 	}
 	return result, nil
