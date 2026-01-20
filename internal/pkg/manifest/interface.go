ca058f707fdf4c54c8759f4e9461ec0f533db44d
Rafael Fonseca <r4f4rfs@gmail.com>
2026-01-20 18:03:29 +0100

    api: use OCI definitions from opencontainers spec
    Instead of implementing our own types.

--- internal/pkg/manifest/interface.go
+++ internal/pkg/manifest/interface.go
@@ -5,17 +5,18 @@ import (
 
 	gcrv1 "github.com/google/go-containerregistry/pkg/v1"
 	digest "github.com/opencontainers/go-digest"
+	specv1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"go.podman.io/image/v5/types"
 
 	"github.com/openshift/oc-mirror/v2/internal/pkg/api/v2alpha1"
 )
 
 type ManifestInterface interface {
-	GetOCIImageIndex(dir string) (*v2alpha1.OCISchema, error)
-	GetOCIImageManifest(file string) (*v2alpha1.OCISchema, error)
+	GetOCIImageIndex(file string) (*specv1.Index, error)
+	GetOCIImageManifest(file string) (*specv1.Manifest, error)
 	GetOCIImageFromIndex(dir string) (gcrv1.Image, error)
 	ExtractOCILayers(img gcrv1.Image, toPath, label string) error
-	ConvertOCIIndexToSingleManifest(dir string, oci *v2alpha1.OCISchema) error
+	ConvertOCIIndexToSingleManifest(dir string, oci *specv1.Index) error
 	GetReleaseSchema(filePath string) ([]v2alpha1.RelatedImage, error)
 	GetOperatorConfig(file string) (*v2alpha1.OperatorConfigSchema, error)
 	ImageDigest(ctx context.Context, sourceCtx *types.SystemContext, imgRef string) (string, error)
