59ae1357354300ee940fa0bb6c965922634984cc
Rafael Fonseca <r4f4rfs@gmail.com>
2026-01-22 16:58:56 +0100

    internal: refactor manifest and archive to reuse common code.
    The code for sanitizing an archive path, writing a stream to a file on
    disk and extracting files from an archive based on a filter can be
    shared between these two places.

--- internal/pkg/archive/unarchive.go
+++ internal/pkg/archive/unarchive.go
@@ -13,6 +13,8 @@ import (
 	"github.com/vbauerster/mpb/v8"
 	"github.com/vbauerster/mpb/v8/decor"
 	"golang.org/x/term"
+
+	tarutils "github.com/openshift/oc-mirror/v2/internal/pkg/archive/utils"
 )
 
 type MirrorUnArchiver struct {
@@ -152,61 +154,12 @@ func (o MirrorUnArchiver) unarchiveChunkTarFile(chunkPath string, bar *mpb.Bar) 
 	return nil
 }
 
-// see https://github.com/securego/gosec/issues/324#issuecomment-935927967
-func sanitizeArchivePath(dir, filePath string) (string, error) {
-	v := filepath.Join(dir, filePath)
-	// OCPBUGS-57387: use absolute paths otherwise the `.` needs special
-	// treatment because of the way Golang handles it after `Clean`
-	absV, err := filepath.Abs(v)
-	if err != nil {
-		return "", fmt.Errorf("get absolute path for %q: %w", v, err)
-	}
-	absDir, err := filepath.Abs(dir)
-	if err != nil {
-		return "", fmt.Errorf("get absolute path for %q: %w", dir, err)
-	}
-	if strings.HasPrefix(absV, absDir+string(os.PathSeparator)) {
-		return v, nil
-	}
-	return "", fmt.Errorf("content filepath is tainted: %s", v)
-}
-
 func createFileWithProgress(parentDir string, header *tar.Header, reader *tar.Reader, bar *mpb.Bar) error {
-	descriptor, err := sanitizeArchivePath(parentDir, header.Name)
+	descriptor, err := tarutils.SanitizeArchivePath(parentDir, header.Name)
 	if err != nil {
 		return err
 	}
 	proxyReader := bar.ProxyReader(reader)
 	defer proxyReader.Close()
-	return writeFile(descriptor, proxyReader, header.FileInfo().Mode(), header.Size)
-}
-
-func writeFile(filePath string, reader io.Reader, perm os.FileMode, size int64) error {
-	// make sure all the parent directories exist
-	descriptorParent := filepath.Dir(filePath)
-	if err := os.MkdirAll(descriptorParent, 0o755); err != nil {
-		return fmt.Errorf("unable to create parent directory for %s: %w", filePath, err)
-	}
-
-	f, err := os.OpenFile(filePath, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, perm)
-	if err != nil {
-		return fmt.Errorf("unable to create file %s: %w", filePath, err)
-	}
-	defer f.Close()
-
-	// copy contents in chunks to avoid gosec:G110 decompression bomb.
-	// https://stackoverflow.com/questions/67327323/g110-potential-dos-vulnerability-via-decompression-bomb-gosec
-	const maxChunkSize = 2048
-	for size > 0 {
-		n, err := io.CopyN(f, reader, maxChunkSize)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			return fmt.Errorf("error copying file %s: %w", filePath, err)
-		}
-		size -= n
-	}
-
-	return nil
+	return tarutils.WriteFile(descriptor, proxyReader, header.FileInfo().Mode(), header.Size)
 }
