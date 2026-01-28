60c0c5ecd6884502929c10172c3999235edb9f49
Dylan Orzel <dorzel@redhat.com>
2026-01-28 11:15:50 -0700

    CLID-198: Add TargetRepo, TargetTag to additionalImages

--- internal/pkg/additional/const.go
+++ internal/pkg/additional/const.go
@@ -3,6 +3,7 @@ package additional
 const (
 	dockerProtocol  = "docker://"
 	ociProtocol     = "oci://"
+	latestTag       = "latest"
 	collectorPrefix = "[AdditionalImagesCollector] "
 	errMsg          = collectorPrefix + "%s"
 )
