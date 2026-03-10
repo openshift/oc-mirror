f501d9e90433955c2ddc3fa05f605782c2a6309d
Josh Branham <jbranham@redhat.com>
2026-03-10 08:38:25 -0600

    fix(cmd): clarify need for platform specific SysProcAttr
    For non-Linux SysProcAttr, add a comment denoting why it is needed, as well
    as tying the child process to the parent.

--- cmd/oc-mirror/sysprocattr_other.go
+++ cmd/oc-mirror/sysprocattr_other.go
@@ -4,6 +4,11 @@ package main
 
 import "syscall"
 
+// sysProcAttr creates a non-linux compatible SysProcAttr, primarily removing the use of
+// Pdeathsig which is only available on Linux. We Setpgid as true to tie the child process
+// to the parent process as a workaround.
 func sysProcAttr() *syscall.SysProcAttr {
-	return &syscall.SysProcAttr{}
+	return &syscall.SysProcAttr{
+		Setpgid: true,
+	}
 }
