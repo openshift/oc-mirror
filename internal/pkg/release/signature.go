ef7896b1eff3f38d204244b3c09a5643ed29c61e
Rafael dos Santos <rdossant@redhat.com>
2026-08-21 01:07:47 +0200

    pkg/release/signature: refactor code and simplify sig verification test
    By having the signature verification code in its own function, we can
    unit test it without having to serve a "release" signature via a mock
    http server.

--- internal/pkg/release/signature.go
+++ internal/pkg/release/signature.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"slices"
 	"strings"
 	"time"
@@ -62,7 +63,11 @@ func NewSignatureClient(log clog.PluggableLoggerInterface, config v2alpha1.Image
 
 // GenerateReleaseSignatures
 func (o SignatureSchema) GenerateReleaseSignatures(ctx context.Context, images []v2alpha1.CopyImageSchema) ([]v2alpha1.CopyImageSchema, error) {
-	var imgs []v2alpha1.CopyImageSchema
+	// OCPBUGS-56009
+	if o.Opts.Global.IgnoreReleaseSignature && len(o.Config.Mirror.Platform.Release) > 0 {
+		return images, nil
+	}
+
 	// set up http object
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: false},
@@ -80,147 +85,41 @@ func (o SignatureSchema) GenerateReleaseSignatures(ctx context.Context, images [
 		}
 	}
 
+	var imgs []v2alpha1.CopyImageSchema
 	for _, img := range images {
-		var data []byte
 		imgSpec, err := image.ParseRef(img.Source)
 		if err != nil {
 			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] parsing image digest")
 		}
 		digest := imgSpec.Digest
 
-		// OCPBUGS-56009
-		// not worrying about code complexity as this module will eventually be deprecated
-		// in favor of the cosign signature work
-		if o.Opts.Global.IgnoreReleaseSignature && len(o.Config.Mirror.Platform.Release) > 0 {
-			imgs = append(imgs, img)
-			continue
-		}
-		// nolint: nestif
+		var data []byte
 		if digest != "" {
 			o.Log.Debug("signature digest %s", digest)
-			// check if the image is in the cache else
-			// do a lookup and download it to cache
-			sigFiles, err := os.ReadDir(o.Opts.Global.WorkingDir + SignatureDir)
-			if err != nil {
-				o.Log.Debug("[GenerateReleaseSignatures] no directory found for signatures %v", err)
-			}
-			for _, file := range sigFiles {
-				if strings.Contains(file.Name(), digest) {
-					data, err = os.ReadFile(o.Opts.Global.WorkingDir + SignatureDir + file.Name())
-					if err != nil {
-						o.Log.Warn("[GenerateReleaseSignatures] could not read %s %v", file.Name(), err)
-					}
-					break
-				}
-			}
-		}
-
-		// we dont have the current digest in cache
-		// nolint: nestif
-		if len(data) == 0 {
-			req, _ := http.NewRequestWithContext(ctx, "GET", signatureURL+"sha256="+digest+"/signature-1", nil)
-			req.Header.Set(ContentType, ApplicationJson)
-			resp, err := httpClient.Do(req)
-			if err != nil {
-				return []v2alpha1.CopyImageSchema{}, fmt.Errorf("http request %w", err)
-			}
-			defer func() {
-				if resp != nil && resp.Body != nil {
-					resp.Body.Close()
-				}
-			}()
-			if resp.StatusCode == http.StatusOK {
-				o.Log.Debug("response from signature lookup %d", resp.StatusCode)
-				data, err = io.ReadAll(resp.Body)
+			// check if the image is in the cache
+			if data = o.getCachedSignature(digest); len(data) == 0 {
+				// we dont have the current digest in cache, do a lookup and download it
+				data, err = o.downloadSignature(ctx, httpClient, signatureURL, digest)
 				if err != nil {
-					return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] reading response body %w", err)
+					return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] failed to get signature for %s: %w", digest, err)
 				}
 			}
 		}
-
 		if len(data) == 0 {
 			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] no signature found for %s image %s", digest, img.Source)
 		}
 
-		pkBytes := []byte(o.pgpKey)
-
-		keyring, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(pkBytes))
-		// keyring, err := openpgp.ReadKeyRing(bytes.NewReader([]byte(pkBytes)))
+		src, err := o.verifySignature(imgSpec.Algorithm, digest, data)
 		if err != nil {
-			o.Log.Error("%v", err)
+			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] invalid signature for %s image %s: %w", digest, img.Source, err)
 		}
-		o.Log.Debug("keyring %v", keyring)
-
-		md, err := openpgp.ReadMessage(bytes.NewReader(data), keyring, nil, nil)
-		if err != nil {
-			o.Log.Error("%v could not read the message:", err)
-		}
-		if md == nil {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] unable to read signature message for %s image %s", digest, img.Source)
-		}
-		if !md.IsSigned {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] message was not signed for %s image %s", digest, img.Source)
-		}
-		if md.SignedBy == nil {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] invalid signature for %s image %s", digest, img.Source)
-		}
-
-		// NOTE: md.SignatureError cannot be trusted until md.UnverifiedBody has been
-		// read to EOF: per the openpgp API, the signature is verified as the body is
-		// consumed, and the check can only complete once the whole message has been
-		// read. So we must fully read UnverifiedBody first, then check SignatureError.
-		//
-		// update the image with the actual reference from the contents json
-		signSchema, err := parser.ParseJsonReader[v2alpha1.SignatureContentSchema](md.UnverifiedBody)
-		if err != nil {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] unmarshal json %w", err)
-		}
-		if md.SignatureError != nil {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] signature error for %s image %s: %w", digest, img.Source, md.SignatureError)
-		}
-		// Sanity: even if the signature is valid, let's double-check it's the signature of the payload we expect.
-		if readDigest := signSchema.Critical.Image.DockerManifestDigest; readDigest != fmt.Sprintf("%s:%s", imgSpec.Algorithm, digest) {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] mismatched digest %q: expected %q", readDigest, digest)
-		}
-		img.Source = signSchema.Critical.Identity.DockerReference
-		o.Log.Debug("image found : %s", signSchema.Critical.Identity.DockerReference)
-
-		o.Log.Trace("field isEncrypted %v", md.IsEncrypted)
-		o.Log.Trace("field EencryptedToKeyIds %v", md.EncryptedToKeyIds)
-		o.Log.Trace("field IsSymmetricallyEncrypted %v", md.IsSymmetricallyEncrypted)
-		o.Log.Trace("field DecryptedWith %v", md.DecryptedWith)
-		o.Log.Trace("field IsSigned %v", md.IsSigned)
-		o.Log.Trace("field SignedByKeyId %v", md.SignedByKeyId)
-		o.Log.Trace("field SignedBy %v", md.SignedBy)
-		o.Log.Trace("field LiteralData %v", md.LiteralData)
-		o.Log.Trace("field SignatureError %v", md.SignatureError)
-		o.Log.Trace("field Signature %v", md.Signature)
-		// o.Log.Trace("field SignatureV3 %v", md.SignatureV3.IssuerKeyId)
-		// o.Log.Trace("field SignatureV3 %v", md.SignatureV3.CreationTime)
-
-		if md.Signature != nil {
-			if md.Signature.SigLifetimeSecs != nil {
-				expiry := md.Signature.CreationTime.Add(time.Duration(*md.Signature.SigLifetimeSecs) * time.Second)
-				if time.Now().After(expiry) {
-					o.Log.Debug("signature expired on %v ", expiry)
-				}
-			}
-		} else if md.SignatureV3 == nil {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] unexpected openpgp.MessageDetails: neither Signature nor SignatureV3 is set for %s image %s", digest, img.Source)
-		}
+		img.Source = src
 
 		// write signature to cache
-		newImgSpec, err := image.ParseRef(img.Source)
-		if err != nil {
-			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] could not parse identity docker reference image %w", err)
+		if err := o.saveSignature(img.Source, digest, data); err != nil {
+			return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] save signature for %s image %s: %w", digest, img.Source, err)
 		}
-		sigFilePath := fmt.Sprintf("%s%s/%s-sha256-%s", o.Opts.Global.WorkingDir, SignatureDir, newImgSpec.Tag, digest)
-		if _, err := os.Stat(sigFilePath); err != nil {
-			ferr := os.WriteFile(sigFilePath, data, 0o600)
-			if ferr != nil {
-				return []v2alpha1.CopyImageSchema{}, fmt.Errorf("[GenerateReleaseSignatures] writing %w", ferr)
-			}
-		}
+
 		// OCPBUGS-52562
 		// add a check to ensure there are no duplicates
 		// ideally it would be better to add this at the early stages of this process
@@ -230,4 +129,139 @@ func (o SignatureSchema) GenerateReleaseSignatures(ctx context.Context, images [
 		}
 	}
 	return imgs, nil
+}
+
+func (o SignatureSchema) downloadSignature(ctx context.Context, client *http.Client, signatureURL string, digest string) ([]byte, error) {
+	req, _ := http.NewRequestWithContext(ctx, "GET", signatureURL+"sha256="+digest+"/signature-1", nil) //nolint:gosec //FIXME: G704: SSRF via taint analysis
+	req.Header.Set(ContentType, ApplicationJson)
+	resp, err := client.Do(req) //nolint:gosec //FIXME: G704: SSRF via taint analysis
+	if err != nil {
+		return nil, fmt.Errorf("http request %w", err)
+	}
+	defer func() {
+		if resp != nil && resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+	o.Log.Debug("response from signature lookup %d", resp.StatusCode)
+	switch resp.StatusCode {
+	case http.StatusOK:
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("read response body %w", err)
+		}
+		return data, nil
+	case http.StatusNotFound:
+		// NOTE: 404 is a special case so we keep the same error message as before
+		return nil, nil
+	default:
+		return nil, fmt.Errorf("unexpected signature request response: %s", resp.Status)
+	}
+}
+
+func (o SignatureSchema) getCachedSignature(digest string) []byte {
+	sigFiles, err := os.ReadDir(filepath.Join(o.Opts.Global.WorkingDir, SignatureDir))
+	if err != nil {
+		o.Log.Debug("[GenerateReleaseSignatures] no directory found for signatures %v", err)
+	}
+
+	var data []byte
+	for _, file := range sigFiles {
+		if !strings.Contains(file.Name(), digest) {
+			continue
+		}
+		data, err = os.ReadFile(filepath.Join(o.Opts.Global.WorkingDir, SignatureDir, file.Name()))
+		if err != nil {
+			o.Log.Warn("[GenerateReleaseSignatures] could not read %s %v", file.Name(), err)
+		}
+		break
+	}
+
+	return data
+}
+
+//nolint:cyclop // not worrying about code complexity as this module will eventually be deprecated in favor of the cosign signature work
+func (o SignatureSchema) verifySignature(algorithm string, digest string, data []byte) (string, error) {
+	pkBytes := []byte(o.pgpKey)
+
+	keyring, err := openpgp.ReadArmoredKeyRing(bytes.NewReader(pkBytes))
+	if err != nil {
+		o.Log.Error("%v", err)
+	}
+	o.Log.Debug("keyring %v", keyring)
+
+	md, err := openpgp.ReadMessage(bytes.NewReader(data), keyring, nil, nil)
+	if err != nil {
+		return "", fmt.Errorf("unable to read signature message: %w", err)
+	}
+	if md == nil {
+		return "", fmt.Errorf("unable to read signature message")
+	}
+	if !md.IsSigned {
+		return "", fmt.Errorf("message was not signed")
+	}
+	if md.SignedBy == nil {
+		return "", fmt.Errorf("invalid signature")
+	}
+
+	// NOTE: md.SignatureError cannot be trusted until md.UnverifiedBody has been
+	// read to EOF: per the openpgp API, the signature is verified as the body is
+	// consumed, and the check can only complete once the whole message has been
+	// read. So we must fully read UnverifiedBody first, then check SignatureError.
+	//
+	// update the image with the actual reference from the contents json
+	signSchema, err := parser.ParseJsonReader[v2alpha1.SignatureContentSchema](md.UnverifiedBody)
+	if err != nil {
+		return "", fmt.Errorf("unmarshal json %w", err)
+	}
+	if md.SignatureError != nil {
+		return "", fmt.Errorf("signature error: %w", md.SignatureError)
+	}
+	// Sanity: even if the signature is valid, let's double-check it's the signature of the payload we expect.
+	if readDigest := signSchema.Critical.Image.DockerManifestDigest; readDigest != fmt.Sprintf("%s:%s", algorithm, digest) {
+		return "", fmt.Errorf("mismatched digest %q: expected %q", readDigest, digest)
+	}
+	imgSource := signSchema.Critical.Identity.DockerReference
+	o.Log.Debug("image found : %s", imgSource)
+
+	o.Log.Trace("field isEncrypted %v", md.IsEncrypted)
+	o.Log.Trace("field EencryptedToKeyIds %v", md.EncryptedToKeyIds)
+	o.Log.Trace("field IsSymmetricallyEncrypted %v", md.IsSymmetricallyEncrypted)
+	o.Log.Trace("field DecryptedWith %v", md.DecryptedWith)
+	o.Log.Trace("field IsSigned %v", md.IsSigned)
+	o.Log.Trace("field SignedByKeyId %v", md.SignedByKeyId)
+	o.Log.Trace("field SignedBy %v", md.SignedBy)
+	o.Log.Trace("field LiteralData %v", md.LiteralData)
+	o.Log.Trace("field SignatureError %v", md.SignatureError)
+	o.Log.Trace("field Signature %v", md.Signature)
+	// o.Log.Trace("field SignatureV3 %v", md.SignatureV3.IssuerKeyId)
+	// o.Log.Trace("field SignatureV3 %v", md.SignatureV3.CreationTime)
+
+	if md.Signature != nil {
+		if md.Signature.SigLifetimeSecs != nil {
+			expiry := md.Signature.CreationTime.Add(time.Duration(*md.Signature.SigLifetimeSecs) * time.Second)
+			if time.Now().After(expiry) {
+				o.Log.Debug("signature expired on %v ", expiry)
+			}
+		}
+	} else if md.SignatureV3 == nil {
+		return "", fmt.Errorf("unexpected openpgp.MessageDetails: neither Signature nor SignatureV3 is set for %s", imgSource)
+	}
+
+	return imgSource, nil
+}
+
+func (o SignatureSchema) saveSignature(imgRef string, digest string, data []byte) error {
+	newImgSpec, err := image.ParseRef(imgRef)
+	if err != nil {
+		return fmt.Errorf("could not parse identity docker reference image %w", err)
+	}
+	sigFilePath := filepath.Join(o.Opts.Global.WorkingDir, SignatureDir, fmt.Sprintf("%s-sha256-%s", newImgSpec.Tag, digest))
+	//nolint:gosec // FIXME: G703: sanitize paths
+	if _, err := os.Stat(sigFilePath); err != nil {
+		if err := os.WriteFile(sigFilePath, data, 0o600); err != nil {
+			return fmt.Errorf("write signature: %w", err)
+		}
+	}
+	return nil
 }
